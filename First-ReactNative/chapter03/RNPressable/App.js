c07063c8188eadf3cc6e8f7e76caf33beb855392
bonjin-all <bonjin.app@gmail.com>
2021-07-15 16:55:05 +0900

    Update App.js: Pressable Component

--- First-ReactNative/chapter03/RNPressable/App.js
+++ First-ReactNative/chapter03/RNPressable/App.js
@@ -7,106 +7,38 @@
  */
 
 import React from 'react';
-import type {Node} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import { View, Text, Pressable } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
+const Button = (props) => {
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+    <Pressable
+      style={{
+        padding: 10, backgroundColor: '#1abc9c'
+      }}
+      onPressIn={() => console.log('onPressIn')}
+      onPressOut={() => console.log('onPressOut')}
+      onPress={() => console.log('onPress')}
+      onLongPress={() => console.log('onLongPress')}
+      delayLongPress={3000}
+      pressRetentionOffset={{ bottom: 50, left: 50, right: 50, top: 50 }}
+      hitSlop={10}
+    >
+      <Text style={{ padding: 10, fontSize: 30 }}>{props.title}</Text>
+    </Pressable>
+  )
+}
+
+const App = () => {
+  return (
+    <View style={{
+      flex: 1,
+      justifyContent: 'center',
+      backgroundColor: '#fff',
+      alignItems: 'center'
+    }}>
+      <Button title="Pressable" />
     </View>
-  );
-};
-
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
+  )
+}
 
 export default App;

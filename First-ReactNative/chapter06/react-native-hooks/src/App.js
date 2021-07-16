25b1ee311c8e6c1c6ce1da105e3c513731b52a98
bonjin-all <bonjin.app@gmail.com>
2021-07-16 15:30:39 +0900

    Add Button.js

---
+++ First-ReactNative/chapter06/react-native-hooks/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.View`
+  flex: 1;
+  background-color: #fff;
+  justify-content: center;
+  align-items: center;
+`;
+
+export default function App() {
+  return (
+    <Container>
+
+    </Container>
+  );
+}

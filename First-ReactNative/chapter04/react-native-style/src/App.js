2fbd1b97e8d08846b1f886d3c884b37fd4b7b633
bonjin-all <bonjin.app@gmail.com>
2021-07-16 09:43:12 +0900

    Add theme.js: ThemeProvider Setting

--- First-ReactNative/chapter04/react-native-style/src/App.js
+++ First-ReactNative/chapter04/react-native-style/src/App.js
@@ -1,24 +1,29 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 import InlineStyle from './components/InlineStyle';
 import ClassStyle from './components/ClassStyle';
 import MultipleStyle from './components/MultipleStyle';
 import OtherStyle from './components/OtherStyle';
 import { Header, Contents, Footer } from './components/Layout';
-import { View } from 'react-native';
+import { View, Switch } from 'react-native';
 import { viewStyles } from './components/styles';
 import ShadowBox from './components/ShadowBox';
-import styled from 'styled-components';
+import styled, { ThemeProvider } from 'styled-components';
 import Button from './components/Button';
 import Input from './components/Input';
+import { theme, lightTheme, darkTheme } from './theme';
 
 const Container = styled.View`
     flex: 1;
-    background-color: #fff;
+    background-color: ${props => props.theme.background}
     align-items: center;
     justify-content: center;
 `;
 export default function App() {
+    const [isDark, setIsDark] = useState(false);
+    const _toggleSwitch = () => setIsDark(!isDark);
+
     return (
+
         // <Fragment>
         //     {/* <InlineStyle /> */}
         //     {/* <ClassStyle /> */}
@@ -40,11 +45,32 @@ export default function App() {
         //     <Button title="GIGAS" />
         //     <Button title="React Native" />
         // </Container>
-        <Container>
-            <Button title="GIGAS" />
-            <Button title="React Native" />
-            <Input borderColor="#3498db" />
-            <Input borderColor="#9b59b6" />
-        </Container>
+
+        // <Container>
+        //     <Button title="GIGAS" />
+        //     <Button title="React Native" />
+        //     <Input borderColor="#3498db" />
+        //     <Input borderColor="#9b59b6" />
+        // </Container>
+
+        // <ThemeProvider theme={theme}>
+        //     <Container>
+        //         <Button title="GIGAS" />
+        //         <Button title="React Native" />
+        //         <Input borderColor="#3498db" />
+        //         <Input borderColor="#9b59b6" />
+        //     </Container>
+        // </ThemeProvider>
+
+        <ThemeProvider theme={isDark ? darkTheme : lightTheme}>
+            <Container>
+                <Switch value={isDark} onValueChange={_toggleSwitch} />
+                <Button title="GIGAS" />
+                <Button title="React Native" />
+                <Input borderColor="#3498db" />
+                <Input borderColor="#9b59b6" />
+            </Container>
+        </ThemeProvider>
     );
 }
+

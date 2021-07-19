8f893781b6ec872746b5cbaa5b3004f8a67839f3
bonjin-all <bonjin.app@gmail.com>
2021-07-19 10:17:36 +0900

    Update Navigator: header style 수정.

--- First-ReactNative/chapter08/react-native-navigation/src/navigations/Stack.js
+++ First-ReactNative/chapter08/react-native-navigation/src/navigations/Stack.js
@@ -10,10 +10,27 @@ const StackNavigation = () => {
     return (
         <Stack.Navigator
             initialRouteName="Home"
-            screenOptions={{ cardStyle: { backgroundColor: '#ffffff' } }}>
+            screenOptions={{
+                cardStyle: { backgroundColor: '#ffffff' },
+                headerStyle: {
+                    height: 110,
+                    backgroundColor: '#95a5a6',
+                    borderBottomWidth: 5,
+                    borderBottomColor: '#34495e'
+                },
+                headerTitleStyle: {
+                    color: '#ffffff',
+                    fontSize: 24,
+                },
+                headerTitleAlign: 'center'
+            }}>
             <Stack.Screen name="Home" component={Home} />
-            <Stack.Screen name="List" component={List} />
-            <Stack.Screen name="Item" component={Item} />
+            <Stack.Screen
+                name="List"
+                component={List}
+                options={{ headerTitle: 'List Screen' }}
+            />
+            <Stack.Screen name="Detail" component={Item} />
         </Stack.Navigator>
     )
 }

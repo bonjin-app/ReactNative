6da7074887686a2403842f072d0d29ce234c4529
bonjin-all <bonjin.app@gmail.com>
2021-07-19 09:48:39 +0900

    Init Project

---
+++ First-ReactNative/chapter08/react-native-navigation/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.View`
+  flex: 1;
+  background-color: #fff;
+  justify-content: center;
+  align-items: center;
+`
+
+const App = () => {
+  return (
+    <Container></Container>
+  )
+}
+
+export default App

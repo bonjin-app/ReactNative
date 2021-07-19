cded6f887fc6612d0e9f56a716b40204712a3fa6
bonjin-all <bonjin.app@gmail.com>
2021-07-19 17:12:39 +0900

    Update Image.js: props rounded 추가.

--- First-ReactNative/chapter09/react-native-simple-chat/src/utils/images.js
+++ First-ReactNative/chapter09/react-native-simple-chat/src/utils/images.js
@@ -3,3 +3,4 @@ const prefix = "https://firebasestorage.googleapis.com/v0/b/react-native-simple-
 export const images = {
     logo: `${prefix}/logo.png?alt=media&token=d2e52d47-e90f-4c87-a0ee-576e54adf8fc`,
 }
+

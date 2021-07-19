c669ecd12d1afa7d3c4d6c18cad3c75c90d05ffb
bonjin-all <bonjin.app@gmail.com>
2021-07-19 11:11:44 +0900

    Update Tab.js: inactive icon 수정.

--- First-ReactNative/chapter08/react-native-navigation/src/navigations/Tab.js
+++ First-ReactNative/chapter08/react-native-navigation/src/navigations/Tab.js
@@ -15,9 +15,8 @@ const Tab = createBottomTabNavigator();
 
 const TabNavigation = () => {
     return (
-        <Tab.Navigator
-            initialRouteName="Meet"
-            screenOptions={({ route }) => ({
+        /**
+         * screenOptions={({ route }) => ({
                 tabBarIcon: (props) => {
                     let name = '';
                     if (route.name === 'Mail') name = 'email'
@@ -26,20 +25,46 @@ const TabNavigation = () => {
                     return TabIcon({ ...props, name })
                 }
             })}
+         */
+        <Tab.Navigator
+            initialRouteName="Meet"
+            tabBarOptions={{
+                labelPosition: 'beside-icon',
+                showLabel: false,
+                style: {
+                    backgroundColor: '#54b7f9',
+                    borderTopColor: '#ffffff',
+                    borderTopWidth: 2
+                },
+                activeTintColor: '#ffffff',
+                inactiveTintColor: '#cfcfcf',
+            }}
         >
             <Tab.Screen
                 name="Mail"
                 component={Mail}
+                options={{
+                    tabBarLabel: 'Inbox',
+                    tabBarIcon: (props) => {
+                        return TabIcon({ ...props, name: props.focused ? 'email' : 'email-outline' })
+                    }
+                }}
             />
             <Tab.Screen
                 name="Meet"
                 component={Meet}
-            />
+                options={{
+                    tabBarIcon: (props) => {
+                        return TabIcon({ ...props, name: props.focused ? 'video' : 'video-outline' })
+                    }
+                }} />
             <Tab.Screen
                 name="Settings"
                 component={Settings}
                 options={{
-                    tabBarIcon: (props) => TabIcon({ ...props, name: 'cog' })
+                    tabBarIcon: (props) => {
+                        return TabIcon({ ...props, name: props.focused ? 'cog' : 'cog-outline' })
+                    }
                 }} />
         </Tab.Navigator>
     )

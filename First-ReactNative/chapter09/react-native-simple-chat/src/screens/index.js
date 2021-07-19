3b68bffa06ea53241cdeabd76b8a6fd63b60f359
bonjin-all <bonjin.app@gmail.com>
2021-07-19 18:43:00 +0900

    Add Screens: Channel, ChannelCreation

--- First-ReactNative/chapter09/react-native-simple-chat/src/screens/index.js
+++ First-ReactNative/chapter09/react-native-simple-chat/src/screens/index.js
@@ -1,4 +1,11 @@
 import Login from "./Login";
 import Signup from "./Signup";
+import Channel from "./Channel";
+import ChannelCreation from "./ChannelCreation";
 
-export { Login, Signup };
+export {
+    Login,
+    Signup,
+    Channel,
+    ChannelCreation,
+};

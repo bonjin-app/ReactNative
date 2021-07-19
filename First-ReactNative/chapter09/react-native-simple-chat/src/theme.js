fd7586ed341b97a226c96389931feb720967d2bd
bonjin-all <bonjin.app@gmail.com>
2021-07-19 11:42:05 +0900

    Add firebase.

---
+++ First-ReactNative/chapter09/react-native-simple-chat/src/theme.js
@@ -0,0 +1,13 @@
+const colors = {
+    white: '#ffffff',
+    black: '#000000',
+    grey0: '#d5d5d5',
+    grey1: '#a6a6a6',
+    red: '#84118',
+    blue: '#3679fe',
+}
+
+export const theme = {
+    background: colors.white,
+    text: colors.black,
+}

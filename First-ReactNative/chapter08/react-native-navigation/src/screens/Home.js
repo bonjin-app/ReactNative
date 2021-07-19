16d235541d7cf1e90fcb0fecccb793d0eae3fbba
bonjin-all <bonjin.app@gmail.com>
2021-07-19 10:41:42 +0900

    Update Stack.js: Header Options

--- First-ReactNative/chapter08/react-native-navigation/src/screens/Home.js
+++ First-ReactNative/chapter08/react-native-navigation/src/screens/Home.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Button } from 'react-native'
 import styled from 'styled-components'
 
-const Container = styled.View`
+const Container = styled.SafeAreaView`
     background-color: #ffffff;
     align-items: center;
 `

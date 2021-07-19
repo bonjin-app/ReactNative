959ef813b30e3743800a49c0b3a501fea349f357
bonjin-all <bonjin.app@gmail.com>
2021-07-19 18:48:05 +0900

    Add MainStack.js

--- First-ReactNative/chapter09/react-native-simple-chat/src/navigations/index.js
+++ First-ReactNative/chapter09/react-native-simple-chat/src/navigations/index.js
@@ -3,12 +3,13 @@ import { NavigationContainer } from '@react-navigation/native';
 import AuthStack from './AuthStack';
 import { Spinner } from '../components';
 import { ProgressContext } from '../contexts';
+import MainStack from './MainStack';
 
 const Navigation = () => {
     const { inProgress } = useContext(ProgressContext);
     return (
         <NavigationContainer>
-            <AuthStack />
+            <MainStack />
             {inProgress &&
                 <Spinner />
             }

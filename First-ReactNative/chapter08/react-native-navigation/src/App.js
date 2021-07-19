91cd51aa7dc3047c64220d501e822be66fca6962
bonjin-all <bonjin.app@gmail.com>
2021-07-19 10:50:04 +0900

    Add Tab Navigator.

--- First-ReactNative/chapter08/react-native-navigation/src/App.js
+++ First-ReactNative/chapter08/react-native-navigation/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import StackNavigation from './navigations/Stack'
+import TabNavigation from './screens/Tab'
 
 const App = () => {
   return (
     <NavigationContainer>
-      <StackNavigation />
+      <TabNavigation />
     </NavigationContainer>
   )
 }

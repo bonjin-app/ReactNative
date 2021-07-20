caed8a899d7e4041ed0402eff9091c3c64647650
bonjin-all <bonjin.app@gmail.com>
2021-07-20 16:54:58 +0900

    Add Navigation

--- ReactNative-Design/foodappui/src/App.js
+++ ReactNative-Design/foodappui/src/App.js
@@ -1,11 +1,13 @@
-import React from 'react'
-import { View } from 'react-native';
+import 'react-native-gesture-handler';
+import * as React from 'react'
+import { NavigationContainer } from '@react-navigation/native';
+import { Text } from 'react-native';
 
 const App = () => {
     return (
-        <View>
-
-        </View>
+        <NavigationContainer>
+            <Text>Welcome to React Native</Text>
+        </NavigationContainer>
     )
 }
 

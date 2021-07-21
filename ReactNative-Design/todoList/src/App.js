01a87b20dba860cc7573bd9a27fbe961e765433e
bonjin-app <bonjin.app@gmail.com>
2021-07-21 17:14:38 +0900

    Update App.js: Input View

--- ReactNative-Design/todoList/src/App.js
+++ ReactNative-Design/todoList/src/App.js
@@ -1,24 +1,37 @@
 import React from 'react'
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native'
+import { KeyboardAvoidingView, Platform, SafeAreaView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
 import Task from './components/Task'
 
 const App = () => {
   return (
     <View style={styles.container}>
       {/* Today's Tasks */}
-      <SafeAreaView>
-        <View style={styles.tasksWrapper}>
+      <View style={styles.tasksWrapper}>
 
-          <Text style={styles.sectionTitle}>Today's tasks</Text>
+        <Text style={styles.sectionTitle}>Today's tasks</Text>
 
-          {/* Items */}
-          <View style={styles.items}>
-            <Task text="Task 1" />
-            <Task text="Task 2" />
-            <Task text="Task 3" />
-          </View>
+        {/* Items */}
+        <View style={styles.items}>
+          <Task text="Task 1" />
+          <Task text="Task 2" />
+          <Task text="Task 3" />
         </View>
-      </SafeAreaView>
+      </View>
+
+
+      {/* Write a task */}
+      <KeyboardAvoidingView
+        behavior={Platform.OS === 'ios' ? "padding" : 'height'}
+        style={styles.writeTaskWrapper}
+      >
+        <TextInput style={styles.input} placeholder="Write a task" />
+
+        <TouchableOpacity>
+          <View style={styles.addWrapper}>
+            <Text style={styles.addText}>+</Text>
+          </View>
+        </TouchableOpacity>
+      </KeyboardAvoidingView>
     </View >
   )
 }
@@ -40,5 +53,35 @@ const styles = StyleSheet.create({
   },
   items: {
     marginTop: 30,
+  },
+  writeTaskWrapper: {
+    position: 'absolute',
+    bottom: 60,
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+  },
+  input: {
+    paddingVertical: 15,
+    paddingHorizontal: 15,
+    backgroundColor: '#fff',
+    borderRadius: 60,
+    borderColor: '#C0C0C0',
+    borderWidth: 1,
+    width: 250,
+  },
+  addWrapper: {
+    width: 60,
+    height: 60,
+    backgroundColor: '#fff',
+    borderRadius: 60,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderColor: '#C0C0C0',
+    borderWidth: 1,
+  },
+  addText: {
+
   },
 })

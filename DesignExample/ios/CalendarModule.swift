cc5b602bad441b8aef6918e54c49b12f62c9d91f
bonjin-app <bonjin.app@gmail.com>
2021-07-21 15:52:29 +0900

    Update Library: ios AlertViewController 추가.

--- DesignExample/ios/CalendarModule.swift
+++ DesignExample/ios/CalendarModule.swift
@@ -6,12 +6,32 @@
 //
 
 import Foundation
+import UIKit
 
 @objc(CalendarModule)
 class CalendarModule: NSObject {
   
   @objc
   func calendar(_ name: String) {
-    print("hihihi")
+    DispatchQueue.main.async { [self] in
+      let rootViewController = UIApplication.shared.delegate?.window??.rootViewController
+      showAlert(viewController: rootViewController!, style: .alert)
+    }
+  }
+  
+  func showAlert(viewController: UIViewController, style: UIAlertController.Style) {
+      let alert = UIAlertController(title: "알림", message: "내용", preferredStyle: style)
+      let success = UIAlertAction(title: "확인", style: .default) { (action) in
+          print("확인")
+      }
+      
+      let cancel = UIAlertAction(title: "취소", style: .cancel, handler: nil)
+      let destructive = UIAlertAction(title: "삭제", style: .destructive)
+      
+      alert.addAction(success)
+      alert.addAction(cancel)
+      alert.addAction(destructive)
+      
+      viewController.present(alert, animated: true, completion: nil)
   }
 }

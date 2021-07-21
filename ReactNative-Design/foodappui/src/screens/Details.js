2cdc819668571760b61554d5aafc78cffe684773
bonjin-app <bonjin.app@gmail.com>
2021-07-21 10:17:18 +0900

    Update Details.js: Info view

--- ReactNative-Design/foodappui/src/screens/Details.js
+++ ReactNative-Design/foodappui/src/screens/Details.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import { SafeAreaView, StyleSheet, Text, View, Image, FlatList, ScrollView } from 'react-native'
+import { SafeAreaView, StyleSheet, Text, View, Image, FlatList, ScrollView, TouchableOpacity } from 'react-native'
 import Icon from 'react-native-vector-icons/Feather';
 import colors from '../../assets/colors/colors';
 
 Icon.loadFont();
 
-export default function Details({ route }) {
+export default function Details({ navigation, route }) {
 
     const { item } = route.params;
 
@@ -14,12 +14,16 @@ export default function Details({ route }) {
             {/* Header */}
             <SafeAreaView>
                 <View style={styles.headerWrapper}>
-                    <View style={styles.headerLeft}>
-                        <Icon
-                            name="chevron-left"
-                            size={12}
-                            color={colors.textDark} />
-                    </View>
+                    <TouchableOpacity
+                        onPress={() => navigation.goBack()}
+                    >
+                        <View style={styles.headerLeft}>
+                            <Icon
+                                name="chevron-left"
+                                size={12}
+                                color={colors.textDark} />
+                        </View>
+                    </TouchableOpacity>
                     <View style={styles.headerRight}>
                         <Icon
                             name="star"
@@ -28,6 +32,45 @@ export default function Details({ route }) {
                     </View>
                 </View>
             </SafeAreaView>
+
+            {/* Titles */}
+            <View style={styles.titlesWrapper}>
+                <Text style={styles.title}>{item.title}</Text>
+            </View>
+
+            {/* Price */}
+            <View style={styles.priceWrapper}>
+                <Text style={styles.priceText}>
+                    $3.99
+                </Text>
+            </View>
+
+            {/* Info */}
+            <View style={styles.infoWrapper}>
+                <View style={styles.infoLeftWrapper}>
+                    <View style={styles.infoIteWrapper}>
+                        <Text style={styles.infoItemTitle}>Size</Text>
+                        <Text style={styles.infoItemText}>
+                            {item.sizeName} {item.sizeNumber}"
+                        </Text>
+                    </View>
+                    <View style={styles.infoIteWrapper}>
+                        <Text style={styles.infoItemTitle}>Crust</Text>
+                        <Text style={styles.infoItemText}>
+                            {item.crust}
+                        </Text>
+                    </View>
+                    <View style={styles.infoIteWrapper}>
+                        <Text style={styles.infoItemTitle}>Delivery In</Text>
+                        <Text style={styles.infoItemText}>
+                            {item.deliveryTime} min
+                        </Text>
+                    </View>
+                </View>
+                <View style={styles.infoRightWrapper}>
+                    <Image source={item.image} style={styles.itemImage} />
+                </View>
+            </View>
         </View>
     )
 }
@@ -56,5 +99,53 @@ const styles = StyleSheet.create({
         borderRadius: 10,
         borderColor: colors.primary,
         borderWidth: 2,
-    }
+    },
+    titlesWrapper: {
+        paddingHorizontal: 20,
+        marginTop: 30,
+    },
+    title: {
+        fontFamily: 'Montserrat-Bold',
+        fontSize: 32,
+        color: colors.textDark,
+        width: '50%',
+    },
+    priceWrapper: {
+        marginTop: 20,
+        paddingHorizontal: 20,
+    },
+    priceText: {
+        color: colors.price,
+        fontFamily: 'Montserrat-Bold',
+        fontSize: 32,
+    },
+    infoWrapper: {
+        marginTop: 60,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+    },
+    infoLeftWrapper: {
+        paddingLeft: 20,
+    },
+    infoIteWrapper: {
+        marginBottom: 20,
+    },
+    infoItemTitle: {
+        fontFamily: 'Montserrat-Medium',
+        fontSize: 14,
+        color: colors.textLight,
+    },
+    infoItemText: {
+        fontFamily: 'Montserrat-SemiBold',
+        fontSize: 18,
+        color: colors.textDark,
+    },
+    infoRightWrapper: {
+
+    },
+    itemImage: {
+        resizeMode: 'contain',
+        marginLeft: 50,
+    },
 });

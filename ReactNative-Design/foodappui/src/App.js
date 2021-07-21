d141a14f9e56daa515f8572a62937fb3b78151bb
bonjin-app <bonjin.app@gmail.com>
2021-07-21 09:59:01 +0900

    Update App.js: Navigation Transition Options

--- ReactNative-Design/foodappui/src/App.js
+++ ReactNative-Design/foodappui/src/App.js
@@ -1,17 +1,48 @@
 import 'react-native-gesture-handler';
 import * as React from 'react'
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import { Text } from 'react-native';
+import { createStackNavigator, TransitionPresets, CardStyleInterpolators, HeaderTitle } from '@react-navigation/stack';
+import { Text, Easing } from 'react-native';
 import Home from './screens/Home';
 import Details from './screens/Details';
 
 const Stack = createStackNavigator();
 
+const config = {
+    animation: 'spring',
+    confing: {
+        stiffness: 1000,
+        damping: 500,
+        mass: 3,
+        overshootClamping: false,
+        restDisplacementThreshold: 0.01,
+        restSpeedThreshold: 0.01,
+    }
+}
+
+const closeConfig = {
+    animation: 'timing',
+    config: {
+        duration: 500,
+        easing: Easing.lenear,
+    }
+}
+
 const App = () => {
     return (
         <NavigationContainer>
-            <Stack.Navigator>
+            <Stack.Navigator
+                screenOptions={{
+                    gestureEnabled: true,
+                    gestureDirection: 'horizontal',
+                    cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS,
+                    // transitionSpec: {
+                    //     open: config,
+                    //     close: closeConfig,
+                    // }
+                    headerTitleAlign: 'center',
+                }}
+            >
                 <Stack.Screen
                     name="Home"
                     component={Home}

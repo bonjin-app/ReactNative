c4e220aa7c04ca775c1a388f49c002fea06319ca
bonjin-app <bonjin.app@gmail.com>
2021-07-21 10:36:33 +0900

    Update Details.js: Ingredients Design

--- ReactNative-Design/foodappui/src/screens/Details.js
+++ ReactNative-Design/foodappui/src/screens/Details.js
@@ -9,68 +9,105 @@ export default function Details({ navigation, route }) {
 
     const { item } = route.params;
 
+    const _renderIngredientsItem = ({ item }) => {
+        return (
+            <View style={[styles.ingredientsItemWrapper, {
+                marginLeft: item.id == 1 ? 20 : 0,
+            }]}>
+                <Image source={item.image} style={styles.ingredientsImage} />
+            </View>
+        )
+    }
+
     return (
         <View style={styles.container}>
-            {/* Header */}
-            <SafeAreaView>
-                <View style={styles.headerWrapper}>
-                    <TouchableOpacity
-                        onPress={() => navigation.goBack()}
-                    >
-                        <View style={styles.headerLeft}>
+            <ScrollView
+                contentInsetAdjustmentBehavior="automatic"
+                showsVerticalScrollIndicator={false}
+            >
+                {/* Header */}
+                <SafeAreaView>
+                    <View style={styles.headerWrapper}>
+                        <TouchableOpacity
+                            onPress={() => navigation.goBack()}
+                        >
+                            <View style={styles.headerLeft}>
+                                <Icon
+                                    name="chevron-left"
+                                    size={12}
+                                    color={colors.textDark} />
+                            </View>
+                        </TouchableOpacity>
+                        <View style={styles.headerRight}>
                             <Icon
-                                name="chevron-left"
+                                name="star"
                                 size={12}
-                                color={colors.textDark} />
+                                color={colors.white} />
                         </View>
-                    </TouchableOpacity>
-                    <View style={styles.headerRight}>
-                        <Icon
-                            name="star"
-                            size={12}
-                            color={colors.white} />
+                    </View>
+                </SafeAreaView>
+
+                {/* Titles */}
+                <View style={styles.titlesWrapper}>
+                    <Text style={styles.title}>{item.title}</Text>
+                </View>
+
+                {/* Price */}
+                <View style={styles.priceWrapper}>
+                    <Text style={styles.priceText}>
+                        $3.99
+                    </Text>
+                </View>
+
+                {/* Info */}
+                <View style={styles.infoWrapper}>
+                    <View style={styles.infoLeftWrapper}>
+                        <View style={styles.infoIteWrapper}>
+                            <Text style={styles.infoItemTitle}>Size</Text>
+                            <Text style={styles.infoItemText}>
+                                {item.sizeName} {item.sizeNumber}"
+                            </Text>
+                        </View>
+                        <View style={styles.infoIteWrapper}>
+                            <Text style={styles.infoItemTitle}>Crust</Text>
+                            <Text style={styles.infoItemText}>
+                                {item.crust}
+                            </Text>
+                        </View>
+                        <View style={styles.infoIteWrapper}>
+                            <Text style={styles.infoItemTitle}>Delivery In</Text>
+                            <Text style={styles.infoItemText}>
+                                {item.deliveryTime} min
+                            </Text>
+                        </View>
+                    </View>
+                    <View style={styles.infoRightWrapper}>
+                        <Image source={item.image} style={styles.itemImage} />
                     </View>
                 </View>
-            </SafeAreaView>
 
-            {/* Titles */}
-            <View style={styles.titlesWrapper}>
-                <Text style={styles.title}>{item.title}</Text>
-            </View>
-
-            {/* Price */}
-            <View style={styles.priceWrapper}>
-                <Text style={styles.priceText}>
-                    $3.99
-                </Text>
-            </View>
-
-            {/* Info */}
-            <View style={styles.infoWrapper}>
-                <View style={styles.infoLeftWrapper}>
-                    <View style={styles.infoIteWrapper}>
-                        <Text style={styles.infoItemTitle}>Size</Text>
-                        <Text style={styles.infoItemText}>
-                            {item.sizeName} {item.sizeNumber}"
-                        </Text>
-                    </View>
-                    <View style={styles.infoIteWrapper}>
-                        <Text style={styles.infoItemTitle}>Crust</Text>
-                        <Text style={styles.infoItemText}>
-                            {item.crust}
-                        </Text>
-                    </View>
-                    <View style={styles.infoIteWrapper}>
-                        <Text style={styles.infoItemTitle}>Delivery In</Text>
-                        <Text style={styles.infoItemText}>
-                            {item.deliveryTime} min
-                        </Text>
+                {/* Ingredients */}
+                <View style={styles.ingredientsWrapper}>
+                    <Text style={styles.ingredientsTitle}>Ingredients</Text>
+                    <View style={styles.ingredientsListWrapper}>
+                        <FlatList
+                            data={item.ingredients}
+                            renderItem={_renderIngredientsItem}
+                            keyExtractor={(item) => item.id}
+                            horizontal={true}
+                            showsHorizontalScrollIndicator={false}
+                        />
                     </View>
                 </View>
-                <View style={styles.infoRightWrapper}>
-                    <Image source={item.image} style={styles.itemImage} />
-                </View>
-            </View>
+
+                {/* Place an order */}
+                <TouchableOpacity onPress={() => alert("Your order has been placed!")}>
+                    <View style={styles.orderWrapper}>
+                        <Text style={styles.orderText}>Place an order</Text>
+                        <Icon name="chevron-right" size={18} color={colors.black} />
+                    </View>
+                </TouchableOpacity>
+            </ScrollView>
         </View>
     )
 }
@@ -141,11 +178,59 @@ const styles = StyleSheet.create({
         fontSize: 18,
         color: colors.textDark,
     },
-    infoRightWrapper: {
-
-    },
     itemImage: {
         resizeMode: 'contain',
         marginLeft: 50,
+    },
+
+    ingredientsWrapper: {
+        marginTop: 40,
+    },
+    ingredientsTitle: {
+        paddingHorizontal: 20,
+        fontFamily: 'Montserrat-SemiBold',
+        fontSize: 16,
+        color: colors.textDark,
+    },
+    ingredientsListWrapper: {
+        paddingVertical: 20,
+    },
+
+    ingredientsItemWrapper: {
+        backgroundColor: colors.white,
+        alignItems: 'center',
+        justifyContent: 'center',
+        paddingHorizontal: 10,
+        marginVertical: 10,
+        marginRight: 15,
+        borderRadius: 15,
+        shadowColor: colors.black,
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.05,
+        shadowRadius: 10,
+        elevation: 2,
+    },
+    ingredientsImage: {
+        resizeMode: 'contain',
+    },
+
+    orderWrapper: {
+        marginTop: 60,
+        marginHorizontal: 20,
+        marginBottom: 20,
+        backgroundColor: colors.primary,
+        borderRadius: 50,
+        paddingVertical: 25,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    orderText: {
+        fontFamily: 'Montserrat-Bold',
+        fontSize: 16,
+        marginRight: 10,
     },
 });

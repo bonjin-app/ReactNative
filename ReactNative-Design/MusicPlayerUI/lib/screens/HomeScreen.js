7870a62be4d8cb9b8f1dbd415691b48aa963119e
bonjin-all <bonjin.app@gmail.com>
2021-07-25 14:19:38 +0900

    Add HomeScreen.js

---
+++ ReactNative-Design/MusicPlayerUI/lib/screens/HomeScreen.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const HomeScreen = () => {
+    return (
+        <View style={styles.container}>
+            <Text></Text>
+        </View>
+    )
+}
+
+export default HomeScreen
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#DEE9FD',
+        alignItems: 'center',
+        justifyContent: 'center',
+    }
+})

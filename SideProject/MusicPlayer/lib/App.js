5877cc17afe458d54dcc0a872cf2aaee5ae19145
bonjin-all <bonjin.app@gmail.com>
2021-07-26 16:56:39 +0900

    Update Fonts

--- SideProject/MusicPlayer/lib/App.js
+++ SideProject/MusicPlayer/lib/App.js
@@ -1,10 +1,22 @@
 import React from 'react'
 import { View, Text } from 'react-native'
+import Icon from 'react-native-vector-icons/MaterialIcons'
 
 const App = () => {
   return (
-    <View>
-      <Text></Text>
+    <View style={{
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'space-around',
+    }}>
+      <Text style={{
+        fontSize: 40,
+        textAlign: 'center',
+      }}>Welcome to {"\n "} CreativeJE</Text>
+      <Icon
+        name="cake"
+        size={170}
+      />
     </View>
   )
 }

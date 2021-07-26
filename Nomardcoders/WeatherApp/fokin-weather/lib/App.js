2caaa2d4827dbea893b93709ec91a48bb2499f14
bonjin-all <bonjin.app@gmail.com>
2021-07-26 14:23:20 +0900

    Add Weather.js: API 통신부 추가.

--- Nomardcoders/WeatherApp/fokin-weather/lib/App.js
+++ Nomardcoders/WeatherApp/fokin-weather/lib/App.js
@@ -1,26 +1,38 @@
 import React, { useEffect, useState } from 'react';
 import { Alert, Platform } from 'react-native';
 import Loading from './screens/Loading';
+import Weather from './screens/Weather';
 import * as Location from 'expo-location';
 import Constants from './utils/Constants';
+import axios from 'axios';
+
 
 export default function App() {
 
   const [isLoading, setIsLoading] = useState(true);
+  const [temp, setTemp] = useState(0);
 
   useEffect(() => {
     getLocation();
 
   }, [])
 
+  const _getWeather = async (latitude, longitude) => {
+    const { data } = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${Constants.API_KEY}&units=metric`)
+    setTemp(data.main.temp)
+    setIsLoading(false)
+  }
+
   const getLocation = async () => {
     try {
       await Location.requestForegroundPermissionsAsync();
       // const location = await (Platform.OS === 'web' ? Location.getCurrentPositionAsync() : Location.getLastKnownPositionAsync());
-      const location = await Location.getCurrentPositionAsync();
+      // const location = await Location.getCurrentPositionAsync();
       const {
         coords: { latitude, longitude }
       } = await Location.getCurrentPositionAsync();
+
+      _getWeather(latitude, longitude)
       setIsLoading(false)      
       
     } catch (error) {
@@ -29,6 +41,6 @@ export default function App() {
   }
 
   return (
-    isLoading ? <Loading/> : null
+    isLoading ? <Loading /> : <Weather temp={Math.round(temp)}/>
   );
 }

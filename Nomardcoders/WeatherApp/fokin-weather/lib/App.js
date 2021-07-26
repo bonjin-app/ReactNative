0e2a9ca6e0262367f337f990e1bd639298601229
bonjin-all <bonjin.app@gmail.com>
2021-07-26 13:54:29 +0900

    Update App.js: Location code. ios14 issue

--- Nomardcoders/WeatherApp/fokin-weather/lib/App.js
+++ Nomardcoders/WeatherApp/fokin-weather/lib/App.js
@@ -1,8 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Alert, Platform } from 'react-native';
 import Loading from './screens/Loading';
+import * as Location from 'expo-location';
 
 export default function App() {
+
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    getLocation();
+
+  }, [])
+
+  const getLocation = async () => {
+    try {
+      await Location.requestForegroundPermissionsAsync();
+      // const location = await (Platform.OS === 'web' ? Location.getCurrentPositionAsync() : Location.getLastKnownPositionAsync());
+      const location = await Location.getCurrentPositionAsync();
+      const {
+        coords: { latitude, longitude }
+      } = await Location.getCurrentPositionAsync();
+      setIsLoading(false)      
+      
+    } catch (error) {
+      Alert.alert(error.message);
+    }
+  }
+
   return (
-    <Loading/>
+    isLoading ? <Loading/> : null
   );
 }

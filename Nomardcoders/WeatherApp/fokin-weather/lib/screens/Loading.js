8fe202e7572fa1a4b5aabbcb2d4d92402e680097
bonjin-all <bonjin.app@gmail.com>
2021-07-26 12:57:09 +0900

    Add Loading.js

---
+++ Nomardcoders/WeatherApp/fokin-weather/lib/screens/Loading.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const Loading = () => {
+    return (
+        <View style={styles.container}>
+            <Text style={styles.text}>Getting the fucking weather</Text>
+        </View>
+    )
+}
+
+export default Loading
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'flex-end',
+        paddingHorizontal: 30,
+        paddingVertical: 100,
+        backgroundColor: '#FDF6AA',
+    },
+    text: {
+        color: '#2C2C2C',
+        fontSize: 30,
+    }
+})

5a444b8637a829c4dede38a86c2028e9b817ef25
bonjin-all <bonjin.app@gmail.com>
2021-08-23 16:35:36 +0900

    Sync

--- Library/ExampleModule/example/ios/File.swift
+++
@@ -1,6 +0,0 @@
-//
-//  File.swift
-//  ExampleModuleExample
-//
-
-import Foundation
--- Library/ExampleModule/src/index.tsx
+++
@@ -1,9 +0,0 @@
-import { NativeModules } from 'react-native';
-
-type ExampleModuleType = {
-  multiply(a: number, b: number): Promise<number>;
-};
-
-const { ExampleModule } = NativeModules;
-
-export default ExampleModule as ExampleModuleType;

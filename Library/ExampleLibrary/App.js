f11962aa7f13e31012a9dd890b8d46bffd6c5ab9
bonjin-all <bonjin.app@gmail.com>
2021-08-23 16:35:01 +0900

    Update Project

--- Library/ExampleLibrary/App.js
+++ Library/ExampleLibrary/App.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import AwesomeModule from 'awesome-native'
+import { Text } from 'react-native'
 
 const App = () => {
+  const [result, setResult] = useState(0)
+
+  useEffect(() => {
+    AwesomeNative.multiply(3, 52)
+                .then(setResult);
+  }, [])
+
   return (
     <div>
       import AwesomeNative from 'awesome-native';
+      <Text>Result: {result}</Text>
     </div>
   )
 }

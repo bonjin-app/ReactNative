f1dd2f80797ca4f83765e061d037cf900ce5cf11
bonjin <bonjin.app@gmail.com>
2021-09-01 18:04:16 +0900

    Sync

--- Library/ExampleCalendarModule/ios/RCTCalendarModule.m
+++ Library/ExampleCalendarModule/ios/RCTCalendarModule.m
@@ -13,7 +13,6 @@ RCT_EXPORT_MODULE();
 
 RCT_EXPORT_METHOD(createCalendarEvent: (NSString *) name
                                       location: (NSString *) location ) {
- 
   RCTLogInfo(@"Pretending to create an event %@ at %@", name, location);
 }
 

0bd5b5a063440fb23e91b4f39c825723d1e68ae7
bonjin-app <bonjin.app@gmail.com>
2021-09-24 10:23:19 +0900

    Add Input

--- ReactNativeSmoothie/MovieApp/src/screens/Login.tsx
+++ ReactNativeSmoothie/MovieApp/src/screens/Login.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import { View, Text } from 'react-native'
+import Input from '~/components/Input'
 
 const Login = () => {
     return (
         <View>
             <Text>Login</Text>
+            <Text>Login</Text>
+            <Text>Login</Text>
+            <Text>Login</Text>
+            <Text>Login</Text>
+            <Input/>
         </View>
     )
 }

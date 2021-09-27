cf99f4d5bd2dafad89b0b764969e97a746c99867
bonjin-app <bonjin.app@gmail.com>
2021-09-27 16:20:39 +0900

    Add Vector Icon config

--- ReactNative-Doit/ch03/ch03_3/App.tsx
+++ ReactNative-Doit/ch03/ch03_3/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import { Image, ImageBackground, SafeAreaView, StyleSheet } from 'react-native'
+import { Image, ImageBackground, Platform, SafeAreaView, StyleSheet, Text, View } from 'react-native'
 import * as D from './src/data'
 
 const avatarUrl = D.randomAvatarUrl();
 const avatarSize = 50
 
-console.log(avatarUrl)
+const text = `Almost before we knew it, we had left the ground.`
 
 const App = () => {
   return (
@@ -14,7 +14,14 @@ const App = () => {
         style={[styles.flex, styles.imageBackground]}
         source={require('./src/assets/images/bg.jpg')}
       >
-        <Image source={{ uri: avatarUrl }} style={[styles.image]}/>
+        <Image source={{ uri: avatarUrl }} style={[styles.image]} />
+        
+        <View style={[styles.flex, styles.padding10]}>
+          <Text style={[styles.text, styles.regular]}>{text} [regular]</Text>
+          <Text style={[styles.text, styles.medium]}>{text} [medium]</Text>
+          <Text style={[styles.text, styles.semiBold]}>{text} [semi bold]</Text>
+          <Text style={[styles.text, styles.bold]}>{text} [bold]</Text>
+        </View>
       </ImageBackground>
     </SafeAreaView>
   )
@@ -33,5 +40,31 @@ const styles = StyleSheet.create({
     width: avatarSize,
     height: avatarSize,
     borderRadius: avatarSize / 2,
+  },
+  padding10: {
+    padding: 10,
+  },
+  text: {
+    textAlign: 'center',
+    fontSize: 25,
+    color: 'white',
+    marginBottom: 10
+  },
+
+  regular: {
+    fontFamily: 'DancingScript-Regular',
+    fontWeight: '400'
+  },
+  medium: {
+    fontFamily: 'DancingScript-Medium',
+    fontWeight: '500'
+  },
+  semiBold: {
+    fontFamily: 'DancingScript-SemiBold',
+    fontWeight: '600'
+  },
+  bold: {
+    fontFamily: 'DancingScript-Bold',
+    fontWeight: Platform.select({ios: '700', android: '600'}),
   }
 })

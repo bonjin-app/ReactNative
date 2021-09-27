e4e874f9170295c78ced411de1aab364a6949a05
bonjin-app <bonjin.app@gmail.com>
2021-09-27 13:39:11 +0900

    Add Person

--- ReactNative-Doit/ch02_3/App.tsx
+++ ReactNative-Doit/ch02_3/App.tsx
@@ -2,12 +2,17 @@ import React from 'react'
 import { SafeAreaView } from 'react-native'
 import ArrowComponent from './src/screens/ArrowComponent'
 import ClassComponent from './src/screens/ClassComponent'
+import Person from './src/screens/Person'
+import * as D from './src/data'
+
+const person = D.createRandomPerson()
 
 const App = () => {
   return (
     <SafeAreaView>
       <ClassComponent />
       <ArrowComponent />
+      <Person person={person} />
     </SafeAreaView>
   )
 }

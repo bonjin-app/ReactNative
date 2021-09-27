d917f312357635ad87b584727644e52564785ba0
bonjin-app <bonjin.app@gmail.com>
2021-09-27 10:58:07 +0900

    Update App.tsx: Debug

--- ReactNative-Doit/ch02_1/App.tsx
+++ ReactNative-Doit/ch02_1/App.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Text } from 'react-native'
 
 export default function App() {
+  console.log('App called');
   const textElement = React.createElement(Text, null, 'Hello world!');
   return textElement;
 }

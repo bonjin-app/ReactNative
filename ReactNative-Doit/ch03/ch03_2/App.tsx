e38dd3416b4992e51e981b80d3016877991109b7
bonjin-app <bonjin.app@gmail.com>
2021-09-27 15:13:19 +0900

    Update App: margin, padding

--- ReactNative-Doit/ch03/ch03_2/App.tsx
+++ ReactNative-Doit/ch03/ch03_2/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { StyleSheet, Text, SafeAreaView, Dimensions, Platform } from 'react-native'
+import { StyleSheet, Text, SafeAreaView, View, Dimensions, Platform } from 'react-native'
 import { Colors } from 'react-native-paper'
 import Color from 'color'
 
@@ -11,6 +11,10 @@ const App = () => {
       <Text style={[styles.text]}>os: { Platform.OS}</Text>
       <Text style={[styles.text]}>width: {width}</Text>
       <Text style={[styles.text]}>height: {height}</Text>
+
+      <View style={[styles.box, styles.border]}></View>
+      <View style={[styles.box, styles.border, {borderRadius: 20}]}></View>
+      <View style={[styles.box, styles.border, {borderTopLeftRadius: 40, borderBottomLeftRadius: 40}]}></View>
     </SafeAreaView>
   )
 }
@@ -21,10 +25,22 @@ export default App
 const styles = StyleSheet.create({
   safeAreaView: {
     backgroundColor: Colors.blue500,
-    height: '50%',
+    flex: 1,
+    padding: 10,
   },
   text: {
     fontSize: 20,
-    color: Color(Colors.blue500).lighten(0.9).string()
+    color: Color(Colors.blue500).lighten(0.9).string(),
+    marginBottom: 10,
+    marginLeft: 30,
+  },
+  box: {
+    height: 100,
+    backgroundColor: Colors.lime500,
+    marginBottom: 10
+  },
+  border: {
+    borderWidth: 10,
+    borderColor: Color('black').alpha(0.3).string(),
   }
 })

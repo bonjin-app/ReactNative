bc5ee846f4db0d3dfc293f0990f68740e897b9a2
bonjin-app <bonjin.app@gmail.com>
2021-09-27 17:52:22 +0900

    Update Content

--- ReactNative-Doit/ch03/ch03_4/src/screens/Content.tsx
+++ ReactNative-Doit/ch03/ch03_4/src/screens/Content.tsx
@@ -8,6 +8,16 @@ const Content = () => {
     return (
         <View style={[styles.view]}>
             <Text style={[styles.text]}>{title}</Text>
+
+            <View style={[{flex: 0, backgroundColor: Colors.red500}]}>
+                <Text>flex: 0</Text>
+            </View>
+            <View style={[{flex: 1, backgroundColor: Colors.green500}]}>
+                <Text>flex: 1</Text>
+            </View>
+            <View style={[{flex: 2, backgroundColor: Colors.purple500}]}>
+                <Text>flex: 2</Text>
+            </View>
         </View>
     )
 }
@@ -16,6 +26,7 @@ export default Content
 
 const styles = StyleSheet.create({
     view: {
+        flex: 1,
         padding: 5,
         backgroundColor: Colors.blue900,
     },

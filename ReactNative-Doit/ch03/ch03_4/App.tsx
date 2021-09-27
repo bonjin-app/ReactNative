878fc75ccc021988dc3c7c6f99da68d823dc540e
bonjin-app <bonjin.app@gmail.com>
2021-09-27 18:22:34 +0900

    Update App: FAB 추가.

--- ReactNative-Doit/ch03/ch03_4/App.tsx
+++ ReactNative-Doit/ch03/ch03_4/App.tsx
@@ -1,17 +1,26 @@
 import React from 'react'
-import { SafeAreaView, StyleSheet } from 'react-native'
+import { Platform, SafeAreaView, StyleSheet, View } from 'react-native'
 import { Colors } from 'react-native-paper'
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons'
 import BottomBar from './src/screens/BottomBar'
 import Content from './src/screens/Content'
 import TopBar from './src/screens/TopBar'
 
 const App = () => {
   return (
-    <SafeAreaView style={styles.flex}>
-      <TopBar />
-      <Content />
-      <BottomBar/>
-    </SafeAreaView>
+    <>
+      
+      <SafeAreaView style={styles.flex}>
+        <TopBar />
+        <Content />
+        <BottomBar />
+      
+      </SafeAreaView>
+       
+      <View style={[styles.absoluteView]}>
+        <Icon name="feather" size={50} color="white"/>
+      </View>
+    </>
   )
 }
 
@@ -19,6 +28,14 @@ const styles = StyleSheet.create({
   flex: {
     flex: 1,
     backgroundColor: Colors.lightBlue100,
+  },
+  absoluteView: {
+    backgroundColor: Colors.purple900,
+    position: 'absolute',
+    right: 30,
+    bottom: Platform.select({ ios: 100, android: 80 }),
+    padding: 10,
+    borderRadius: 35,
   }
 })
 

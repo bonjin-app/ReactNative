83815076b0f2c183fe0bc014fb60205b7802f992
bonjin-app <bonjin.app@gmail.com>
2021-09-27 14:48:37 +0900

    Init ch03_2 Project

--- ReactNative-Doit/ch03/ch03_1/App.tsx
+++ ReactNative-Doit/ch03/ch03_1/App.tsx
@@ -1,10 +1,15 @@
+import Color from 'color'
 import React from 'react'
 import { SafeAreaView, Text, StyleSheet } from 'react-native'
+import { Colors } from 'react-native-paper'
+
+console.log(Colors.blue500)
+console.log(Color(Colors.blue500).alpha(0.5).lighten(0.5).string())
 
 const App = () => {
   return (
-    <SafeAreaView style={[styles.safeAreaView, { backgroundColor: 'blue' }]}>
-      <Text style={[styles.text, {color: 'white'}]}>Hello world!</Text>
+    <SafeAreaView style={[styles.safeAreaView]}>
+      <Text style={[styles.text]}>Hello world!</Text>
     </SafeAreaView>
   )
 }
@@ -13,9 +18,11 @@ const styles = StyleSheet.create({
   safeAreaView: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'center'
+    justifyContent: 'center',
+    backgroundColor: Colors.blue500
   },
   text: {
+    color: Color(Colors.blue500).alpha(0.7).lighten(0.9).string(),
     fontSize: 20
   }
 })

a6f4c80070bacfd6b5c3f0d6f2196f9edadf562d
bonjin-app <bonjin.app@gmail.com>
2021-09-28 21:30:18 +0900

    Update Home. useTheme

--- ReactNative-Doit/ch05/ch05_1/src/screens/Home.tsx
+++ ReactNative-Doit/ch05/ch05_1/src/screens/Home.tsx
@@ -1,13 +1,26 @@
+import Color from 'color'
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
-import { Colors } from 'react-native-paper'
+import { Colors, useTheme } from 'react-native-paper'
 import * as D from '../data'
 
-const title = 'Home'
 const Home = () => {
+    const theme = useTheme()
+    const { fonts, colors } = theme
+
     return (
-        <View style={[styles.view]}>
-            <Text style={[styles.text]}>{title}</Text>
+        <View style={[styles.view, { backgroundColor: colors.background }]}>
+            <View style={[styles.bar, { backgroundColor: colors.primary }]}>
+                <Text style={[styles.text, { color: colors.text }, fonts.medium]}>TopBar</Text>
+            </View>
+            <View style={styles.content}>
+                <Text style={[styles.text, { color: colors.text }, fonts.regular]}>Welcome to Context world!</Text>
+            </View>
+            <View style={[styles.bar, { backgroundColor: colors.accent }]}>
+                <Text style={[styles.text, { color: colors.text }, fonts.light]}>
+                    BottomBar
+                </Text>
+            </View>
         </View>
     )
 }
@@ -19,6 +32,17 @@ const styles = StyleSheet.create({
         flex: 1,
         padding: 5,
         backgroundColor: Colors.blue900,
+    },
+    bar: {
+        height: 50,
+        flexDirection: 'row',
+        padding: 5,
+        alignItems: 'center',
+    },
+    content: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
     },
     text: {
         fontSize: 20,

b955dd9bd918d4080b3dd87c44b0336d552b407b
bonjin-app <bonjin.app@gmail.com>
2021-09-28 15:23:25 +0900

    Update Timer: Loading 추가.

--- ReactNative-Doit/ch04/ch04_4/src/screens/Timer.tsx
+++ ReactNative-Doit/ch04/ch04_4/src/screens/Timer.tsx
@@ -1,13 +1,28 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React, { useCallback, useEffect, useState } from 'react'
+import { Button, StyleSheet, ActivityIndicator, Text, View } from 'react-native'
 import { Colors } from 'react-native-paper'
 import * as D from '../data'
 
-const title = 'Timer'
 const Timer = () => {
+    const [loading, setLoading] = useState(true)
+    const toggleLoading = useCallback(() => setLoading((loading) => !loading), [])
+
+    useEffect(() => {
+        const id = setTimeout(() => setLoading(false), 3000)
+        return () => {
+            clearInterval(id)
+        }
+    }, [loading])
+
     return (
         <View style={[styles.view]}>
-            <Text style={[styles.text]}>{title}</Text>
+            <Text style={[styles.title]}>Timer</Text>
+            <Text>laoding: {loading.toString()}</Text>
+            
+            <Button onPress={toggleLoading} title={loading ? 'stop loading' : 'start loading'} />
+            {loading && (
+                <ActivityIndicator size='large' color={ Colors.deepPurple700}/>
+            )}
         </View>
     )
 }
@@ -17,11 +32,11 @@ export default Timer
 const styles = StyleSheet.create({
     view: {
         flex: 1,
-        padding: 5,
-        backgroundColor: Colors.blue900,
+        alignItems: 'center',
+        backgroundColor: Colors.yellow300,
     },
-    text: {
+    title: {
         fontSize: 20,
-        color: 'white',
+        fontWeight: '600',
     }
 })

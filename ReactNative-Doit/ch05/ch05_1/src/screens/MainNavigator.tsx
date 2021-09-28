3ccb1828147d1302c7da758429653aed6d8da6be
bonjin-app <bonjin.app@gmail.com>
2021-09-28 17:31:17 +0900

    Update MainNavigator

--- ReactNative-Doit/ch05/ch05_1/src/screens/MainNavigator.tsx
+++ ReactNative-Doit/ch05/ch05_1/src/screens/MainNavigator.tsx
@@ -1,13 +1,18 @@
-import React, { useState } from 'react'
+import React, { useMemo, useState } from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 import { BottomNavigation } from 'react-native-paper'
+import Home from './Home'
 
 const MainNavigator = () => {
     const [index, setIndex] = useState<number>(0)
-    const [routes] = useState([])
+    const routes = useMemo(() => [{
+        key: 'home',
+        title: 'Home',
+        icon: 'home'
+    }], [])
 
     const renderScene = BottomNavigation.SceneMap({
-
+        home: Home
     })
 
     return (

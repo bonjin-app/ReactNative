1dbcd9dcb51a601ac5cb5e142facc216bff6c449
bonjin-app <bonjin.app@gmail.com>
2021-09-28 16:53:51 +0900

    Update Interval

--- ReactNative-Doit/ch04/ch04_5/src/screens/Interval.tsx
+++ ReactNative-Doit/ch04/ch04_5/src/screens/Interval.tsx
@@ -4,6 +4,7 @@ import { ScrollView, StyleSheet, Text, View } from 'react-native'
 import { Colors } from 'react-native-paper'
 import Avatar from '../components/Avatar'
 import * as D from '../data'
+import { useInterval } from '../hooks'
 
 type IdAndAvatar = Pick<D.IPerson, 'id' | 'avatar'>
 
@@ -13,20 +14,14 @@ const Interval = () => {
     const toggleStart = useCallback(() => setStart((start) => !start), [])
     const clearAvatars = useCallback(() => setAvatars((notUsed) => []), [])
 
-    useEffect(() => {
-        const id = setInterval(() => {
-            if (start) {
-                setAvatars((avatars) => [
-                    ...avatars,
-                    { id: D.randomId(), avatar: D.randomAvatarUrl() }
-                ])
-            }
-        }, 1000)
-
-        return () => {
-            clearInterval(id)
+    useInterval(() => {
+        if (start) {
+            setAvatars((avatars) => [
+                ...avatars,
+                { id: D.randomId(), avatar: D.randomAvatarUrl() }
+            ])
         }
-    }, [start])
+    }, 1000, [start])
 
     const children = avatars.map(({ id, avatar }) => (
         <Avatar key={id} uri={avatar} size={70} viewStyle={ styles.avatarViewStyle}/>

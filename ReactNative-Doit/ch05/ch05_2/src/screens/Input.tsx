9783f0b68d90a06e104c5d5ef00b7f943c2a26bf
bonjin-app <bonjin.app@gmail.com>
2021-09-29 08:55:03 +0900

    Update Input: base 코드 작성.

--- ReactNative-Doit/ch05/ch05_2/src/screens/Input.tsx
+++ ReactNative-Doit/ch05/ch05_2/src/screens/Input.tsx
@@ -1,13 +1,32 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
-import { Colors } from 'react-native-paper'
+import React, { useState } from 'react'
+import { StyleSheet, Switch, Text, View } from 'react-native'
+import { Colors, TextInput, useTheme } from 'react-native-paper'
+import { useToggleTheme } from '../contexts'
 import * as D from '../data'
 
 const title = 'Input'
 const Input = () => {
+    const [person, setPerson] = useState<D.IPerson>(D.createRandomPerson())
+    const { dark, colors } = useTheme()
+    const toggleTheme = useToggleTheme()
+
     return (
-        <View style={[styles.view]}>
-            <Text style={[styles.text]}>{title}</Text>
+        <View style={[styles.view, { backgroundColor: colors.surface }]}>
+            <View style={[styles.topBar, { backgroundColor: colors.accent }]}>
+                <Text style={[styles.textButton]}>focus</Text>
+                <Text style={[styles.textButton]}>dismiss keyboard</Text>
+                <View style={{ flex: 1 }}></View>
+                <Switch value={dark} onValueChange={toggleTheme} />
+            </View>
+            <View style={[styles.textView]}>
+                <Text style={[styles.text, { color: colors.text }]}>email</Text>
+                <TextInput
+                    style={[styles.textInput, { color: colors.text, borderColor: colors.placeholder }]}
+                    value={person.email}
+                    placeholder='enter your email'
+                    onChangeText={(email) => setPerson((person) => ({ ...person, email }))}
+                />
+            </View>
         </View>
     )
 }
@@ -17,11 +36,28 @@ export default Input
 const styles = StyleSheet.create({
     view: {
         flex: 1,
-        padding: 5,
-        backgroundColor: Colors.blue900,
+    },
+    topBar: {
+        flexDirection: 'row',
+        padding: 5
+    },
+    textButton: {
+        marginLeft: 10,
+        fontSize: 20
+    },
+    keyboardAvoidingView: {
+        flex: 1,
+        padding: 10,
+    },
+    textView: {
+        padding: 5
     },
     text: {
-        fontSize: 20,
-        color: 'white',
+        fontSize: 24,
+    },
+    textInput: {
+        fontSize: 24,
+        borderWidth: 1,
+        borderRadius: 5
     }
 })

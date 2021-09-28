7230333e3c8172aecb9e9ab7b3ae81014d505ca9
bonjin-app <bonjin.app@gmail.com>
2021-09-28 15:36:59 +0900

    Update Interval

--- ReactNative-Doit/ch04/ch04_4/src/screens/Interval.tsx
+++ ReactNative-Doit/ch04/ch04_4/src/screens/Interval.tsx
@@ -1,13 +1,47 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { identifier } from '@babel/types'
+import React, { useCallback, useEffect, useState } from 'react'
+import { ScrollView, StyleSheet, Text, View } from 'react-native'
 import { Colors } from 'react-native-paper'
+import Avatar from '../components/Avatar'
 import * as D from '../data'
 
-const title = 'Interval'
+type IdAndAvatar = Pick<D.IPerson, 'id' | 'avatar'>
+
 const Interval = () => {
+    const [avatars, setAvatars] = useState<IdAndAvatar[]>([])
+    const [start, setStart] = useState(true)
+    const toggleStart = useCallback(() => setStart((start) => !start), [])
+    const clearAvatars = useCallback(() => setAvatars((notUsed) => []), [])
+
+    useEffect(() => {
+        const id = setInterval(() => {
+            if (start) {
+                setAvatars((avatars) => [
+                    ...avatars,
+                    { id: D.randomId(), avatar: D.randomAvatarUrl() }
+                ])
+            }
+        }, 1000)
+
+        return () => {
+            clearInterval(id)
+        }
+    }, [start])
+
+    const children = avatars.map(({ id, avatar }) => (
+        <Avatar key={id} uri={avatar} size={70} viewStyle={ styles.avatarViewStyle}/>
+    ))
+
     return (
         <View style={[styles.view]}>
-            <Text style={[styles.text]}>{title}</Text>
+            <View style={[styles.topBar]}>
+                <Text onPress={ toggleStart} style={ styles.topBarText}>{start ? 'stop' : 'start'}</Text>
+                <Text onPress={ clearAvatars} style={ styles.topBarText}>clear avatars</Text>
+            </View>
+            <Text style={[styles.title]}>Interval</Text>
+            <ScrollView contentContainerStyle={ styles.contentContainerStyle}>
+                {children}
+            </ScrollView>
         </View>
     )
 }
@@ -17,11 +51,31 @@ export default Interval
 const styles = StyleSheet.create({
     view: {
         flex: 1,
-        padding: 5,
-        backgroundColor: Colors.blue900,
+        alignItems: 'center',
+        backgroundColor: Colors.lime300,
     },
-    text: {
+    title: {
+        fontSize: 30,
+        fontWeight: '600',
+    },
+    topBar: {
+        width: '100%',
+        flexDirection: 'row',
+        padding: 5,
+        justifyContent: 'space-between',
+        backgroundColor: Colors.blue900
+
+    },
+    topBarText: {
         fontSize: 20,
-        color: 'white',
+        color: 'white'
+    },
+    contentContainerStyle: {
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        justifyContent: 'center',
+    },
+    avatarViewStyle: {
+        padding: 5
     }
 })

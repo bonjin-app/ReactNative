19896f0e1c7a7b5cbba17db94294d2a8bc5e59cc
bonjin-app <bonjin.app@gmail.com>
2021-09-28 15:00:51 +0900

    Update LifeCycle

--- ReactNative-Doit/ch04/ch04_4/src/screens/LifeCycle.tsx
+++ ReactNative-Doit/ch04/ch04_4/src/screens/LifeCycle.tsx
@@ -1,13 +1,35 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React, { useCallback, useEffect, useLayoutEffect } from 'react'
+import { LayoutChangeEvent, Platform, StyleSheet, Text, View } from 'react-native'
 import { Colors } from 'react-native-paper'
 import * as D from '../data'
 
-const title = 'LifeCycle'
 const LifeCycle = () => {
+    useEffect(() => {
+        console.log(Platform.OS, 'useEffect called')
+        
+        return () => {
+            console.log(Platform.OS, 'useEffect finished')
+        };
+    })
+
+    useLayoutEffect(() => {
+        console.log(Platform.OS, 'useLayoutEffect called')
+
+        return () => {
+            console.log(Platform.OS, 'useLayoutEffect finished')
+        };
+    }, [])
+
+    const onLayout = useCallback((e: LayoutChangeEvent) => {
+        const { layout } = e.nativeEvent
+        console.log(Platform.OS, 'onLayout called', layout)
+    }, [])
+
+    console.log(Platform.OS, 'render start')
+
     return (
-        <View style={[styles.view]}>
-            <Text style={[styles.text]}>{title}</Text>
+        <View onLayout={ onLayout} style={[styles.view]}>
+            <Text style={[styles.text]}>LifeCycle</Text>
         </View>
     )
 }
@@ -17,11 +39,11 @@ export default LifeCycle
 const styles = StyleSheet.create({
     view: {
         flex: 1,
-        padding: 5,
-        backgroundColor: Colors.blue900,
+        alignItems: 'center',
+        backgroundColor: Colors.blue100
     },
     text: {
-        fontSize: 20,
-        color: 'white',
+        fontSize: 30,
+        fontWeight: '600',
     }
 })

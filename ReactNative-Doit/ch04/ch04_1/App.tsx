d002a6d976c59a13bf471f91d1176c488fb27095
bonjin-app <bonjin.app@gmail.com>
2021-09-28 10:35:35 +0900

    Add useClock

--- ReactNative-Doit/ch04/ch04_1/App.tsx
+++ ReactNative-Doit/ch04/ch04_1/App.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native'
+import { SafeAreaView, StyleSheet, Text} from 'react-native'
+import { useClock } from './src/hooks'
 
 const App = () => {
-  const time = new Date()
+  const time = useClock()
   
   return (
     <SafeAreaView style={[styles.safeAreaView]}>

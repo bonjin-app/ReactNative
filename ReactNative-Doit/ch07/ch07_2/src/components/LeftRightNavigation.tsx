0a305501340a4665b66f565d2fdb4df11cbf43b1
bonjin-app <bonjin.app@gmail.com>
2021-09-29 15:37:33 +0900

    Update LeftRightNavigation

--- ReactNative-Doit/ch07/ch07_2/src/components/LeftRightNavigation.tsx
+++ ReactNative-Doit/ch07/ch07_2/src/components/LeftRightNavigation.tsx
@@ -1,11 +1,12 @@
-import { forwardRef, ForwardRefRenderFunction, MutableRefObject, ReactNode, useImperativeHandle, useState } from "react"
-import { Animated, FlatList, Platform, StyleSheet } from "react-native"
-import { usePanResponder } from "../hooks"
+import React, { useState, forwardRef, useImperativeHandle } from 'react'
+// prettier-ignore
+import type { ForwardRefRenderFunction, ReactNode, MutableRefObject } from 'react'
+import { Platform, StyleSheet, Animated, FlatList, View } from 'react-native'
+import { usePanResponder } from '../hooks'
 
 export type LeftRightNavigationMethods = {
     resetOffset: () => void
 }
-
 export type LeftRightNavigationProps = {
     onRightToLeft?: () => void
     onLeftToRight?: () => void
@@ -14,47 +15,51 @@ export type LeftRightNavigationProps = {
     flatListRef?: MutableRefObject<FlatList | null>
 }
 
-const _LeftRightNavigation: ForwardRefRenderFunction<LeftRightNavigationMethods, LeftRightNavigationProps> = (
-    { distance, children, onRightToLeft, onLeftToRight, flatListRef }, ref) => {
-
+const _LeftRightNavigation: ForwardRefRenderFunction<
+    LeftRightNavigationMethods,
+    LeftRightNavigationProps
+> = ({ distance, children, onRightToLeft, onLeftToRight, flatListRef }, ref) => {
     const [offset, setOffset] = useState<number>(0)
-
-    useImperativeHandle(ref, () => ({
-        resetOffset() {
-            setOffset(0)
-        }
-    }), [])
-
-    const panResponder = usePanResponder({
-        onPanResponderMove(e, s) {
-            const { dx, dy } = s
-            if (flatListRef) {
-                flatListRef.current?.scrollToOffset({
-                    offset: offset - dy,
-                    animated: false
-                })
-                setOffset((offset) => offset - dy)
+    useImperativeHandle(
+        ref,
+        () => ({
+            resetOffset() {
+                setOffset(0)
             }
-
-            if (Math.abs(dy) < 30) {
-                if (dx > distance) {
-                    onLeftToRight && onLeftToRight()
-                } else if (dx < -distance) {
-                    onRightToLeft && onRightToLeft()
+        }),
+        []
+    )
+    const panResponder = usePanResponder(
+        {
+            onPanResponderMove(e, s) {
+                const { dx, dy } = s
+                if (flatListRef) {
+                    flatListRef.current?.scrollToOffset({
+                        offset: offset - dy,
+                        animated: false
+                    })
+                    setOffset(offset => offset - dy)
+                }
+                if (Math.abs(dy) < 30) {
+                    // 수직 스크롤이 거의 없을 때만 적용
+                    if (dx > distance) {
+                        // 왼쪽에서 오른쪽 제스처 길이
+                        onLeftToRight && onLeftToRight()
+                    } else if (dx < -distance) {
+                        // 오른쪽에서 왼쪽 제스처 길이
+                        onRightToLeft && onRightToLeft()
+                    }
                 }
             }
-        }
-    }, [offset])
-
+        },
+        [offset]
+    )
     return (
-        <Animated.View
-            style={[styles.view]}
-            {...panResponder.panHandlers}>
+        <Animated.View style={[styles.view]} {...panResponder.panHandlers}>
             {children}
         </Animated.View>
     )
 }
-
 export const LeftRightNavigation = forwardRef(_LeftRightNavigation)
 
 const styles = StyleSheet.create({
--- ReactNative-Doit/ch07/ch07_2/src/screens/Home.tsx
+++ ReactNative-Doit/ch07/ch07_2/src/screens/Home.tsx
@@ -1,7 +1,8 @@
 import { useNavigation } from '@react-navigation/core'
-import React, { useState, useCallback, useEffect } from 'react'
+import React, { useState, useCallback, useEffect, useRef } from 'react'
 import { StyleSheet, View, Text, Switch, FlatList } from 'react-native'
 import { useTheme } from 'react-native-paper'
+import { LeftRightNavigation, LeftRightNavigationMethods } from '../components'
 import { useToggleTheme } from '../contexts'
 import { ScrollEnabledProvider, useScrollEnabled } from '../contexts'
 import * as D from '../data'
@@ -22,20 +23,23 @@ export default function Home() {
   // for people
   const [scrollEnabled] = useScrollEnabled()
   const [people, setPeople] = useState<D.IPerson[]>([])
-  const theme = useTheme()
-  const toggleTheme = useToggleTheme()
+
+  const leftRef = useRef<LeftRightNavigationMethods | null>(null)
+
   const addPerson = useCallback(() => {
     setPeople(people => [D.createRandomPerson(), ...people])
   }, [])
   const removeAllPersons = useCallback(() => {
     setPeople(notUsed => [])
   }, [])
-  const deletePerson = useCallback(
-    (id: string) => () =>
-      setPeople(people => people.filter(person => person.id != id)),
-    []
-  )
+  const deletePerson = useCallback((id: string) => () => {
+    setPeople(people => people.filter(person => person.id != id)),
+      leftRef.current?.resetOffset(),
+      flatListRef.current?.scrollToOffset({ animated: true, offset: 0 })
+  }, [])
   // useEffect(addPerson, [])
+
+  const flatListRef = useRef<FlatList | null>(null)
 
   useEffect(() => {
     D.makeArray(5).forEach(addPerson)
@@ -44,7 +48,7 @@ export default function Home() {
   return (
     <SafeAreaView>
       <ScrollEnabledProvider>
-        <View style={[styles.view, { backgroundColor: theme.colors.surface }]}>
+        <View style={[styles.view]}>
           <TopBar>
             <UnderlineText onPress={goLeft} style={styles.text}>go Left</UnderlineText>
             <UnderlineText onPress={goRight} style={styles.text}>go Right</UnderlineText>
@@ -57,14 +61,23 @@ export default function Home() {
               remove all
             </UnderlineText>
           </TopBar>
-          <FlatList
-            scrollEnabled={scrollEnabled}
-            data={people}
-            renderItem={({ item }) => (
-              <Person person={item} deletePressed={deletePerson(item.id)} />
-            )}
-            keyExtractor={item => item.id}
-          />
+
+          <LeftRightNavigation
+            ref={leftRef} distance={40}
+            flatListRef={flatListRef}
+            onLeftToRight={goLeft}
+            onRightToLeft={goRight}
+          >
+            <FlatList
+              ref={flatListRef}
+              scrollEnabled={scrollEnabled}
+              data={people}
+              renderItem={({ item }) => (
+                <Person person={item} deletePressed={deletePerson(item.id)} />
+              )}
+              keyExtractor={item => item.id}
+            />
+          </LeftRightNavigation>
         </View>
       </ScrollEnabledProvider>
     </SafeAreaView>

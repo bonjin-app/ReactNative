20a61c58f9578b5d7ed072cfa66022c19aa9d63d
bonjin-app <bonjin.app@gmail.com>
2021-09-29 13:09:46 +0900

    Update CopyMe

--- ReactNative-Doit/ch07/ch07_1/src/copy/CopyMe.tsx
+++ ReactNative-Doit/ch07/ch07_1/src/copy/CopyMe.tsx
@@ -1,17 +1,26 @@
 import React from 'react'
-import {StyleSheet, View, Text} from 'react-native'
-import {Colors} from 'react-native-paper'
-import * as D from '../data'
+import { StyleSheet, View, Text } from 'react-native'
+import { SafeAreaView, TopBar } from '../theme/navigation'
 
 const title = 'CopyMe'
 export default function CopyMe() {
   return (
-    <View style={[styles.view]}>
-      <Text style={[styles.text]}>{title}</Text>
-    </View>
+    <SafeAreaView style={[styles.view]}>
+      <View style={[styles.view]}>
+        <TopBar />
+        <View style={[styles.content]}>
+          <Text style={[styles.text]}>{title}</Text>
+        </View>
+      </View>
+    </SafeAreaView>
   )
 }
 const styles = StyleSheet.create({
-  view: {flex: 1, padding: 5, backgroundColor: Colors.blue900},
-  text: {fontSize: 20, color: 'white'}
+  view: { flex: 1, padding: 5 },
+  content: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  text: { fontSize: 20 }
 })

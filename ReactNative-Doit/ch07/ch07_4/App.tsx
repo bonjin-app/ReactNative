80853c3f5b29fb904469b09ebc6536938fc81e79
bonjin-app <bonjin.app@gmail.com>
2021-09-29 20:26:50 +0900

    Update Base

--- ReactNative-Doit/ch07/ch07_4/App.tsx
+++ ReactNative-Doit/ch07/ch07_4/App.tsx
@@ -1,115 +1,36 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
- *
- * @format
- */
+import React, { useCallback, useState } from 'react'
+import { NavigationContainer, DarkTheme, DefaultTheme } from '@react-navigation/native'
+import { enableScreens } from 'react-native-screens'
+import { SafeAreaProvider } from 'react-native-safe-area-context'
 
-import React from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import MainNavigator from './src/screens/MainNavigator'
+import { AppearanceProvider } from 'react-native-appearance'
+import { ToggleThemeProvider } from './src/contexts'
+import { useColorScheme } from 'react-native'
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const Section: React.FC<{
-  title: string;
-}> = ({children, title}) => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
+enableScreens()
 
 const App = () => {
-  const isDarkMode = useColorScheme() === 'dark';
+  const scheme = useColorScheme()
+  const [theme, setTheme] = useState(scheme === 'dark' ? DarkTheme : DefaultTheme)
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+  const toggleTheme = useCallback(() => {
+    setTheme(({ dark }) => (
+      dark ? DefaultTheme : DarkTheme
+    ))
+  }, [])
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
+    <AppearanceProvider>
+      <ToggleThemeProvider toggleTheme={toggleTheme}>
+        <SafeAreaProvider>
+          <NavigationContainer theme={theme}>
+            <MainNavigator />
+          </NavigationContainer>
+        </SafeAreaProvider>
+      </ToggleThemeProvider>
+    </AppearanceProvider>
+  )
+}
 
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
-
-export default App;
+export default App
--- ReactNative-Doit/ch07/ch07_4/src/screens/TabNavigator.tsx
+++ ReactNative-Doit/ch07/ch07_4/src/screens/TabNavigator.tsx
@@ -9,6 +9,7 @@ import SignUp from './SignUp'
 import HomeNavigator from './HomeNavigator'
 
 import type { RouteProp, ParamListBase } from '@react-navigation/native'
+import { Colors } from 'react-native-paper'
 
 type TabBarIconProps = {
     focused: boolean
@@ -16,17 +17,20 @@ type TabBarIconProps = {
     size: number
 }
 
+const icons: Record<string, string[]> = {
+    HomeNavigator: ['home-circle', 'home-circle-outline']
+}
+
 const screenOptions = ({ route }: { route: RouteProp<ParamListBase, string> }) => {
     return {
+        headerShown: false,
         tabBarIcon: ({ focused, color, size }: TabBarIconProps) => {
             const { name } = route
-            switch (name) {
-                case 'Login':
-                    return <AntIcon name="login" size={size} color={color} />
-                case 'SignUp':
-                    return <FontawesomeIcon name="sign-in" size={size} color={color} />
-            }
-            return <Icon name="home" size={size} color={color} />
+            const focusedSize = focused ? size + 6 : size
+            const focusedColor = focused ? Colors.lightBlue500 : color
+            const [icon, iconOutline] = icons[name]
+            const iconName = focused ? icon : iconOutline
+            return <Icon name={iconName} size={size} color={color} />
         }
     }
 }
@@ -36,9 +40,7 @@ const Tab = createBottomTabNavigator()
 export default function TabNavigator() {
     return (
         <Tab.Navigator screenOptions={screenOptions}>
-            <Tab.Screen name="Login" component={Login} />
-            <Tab.Screen name="SignUp" component={SignUp} />
-            <Tab.Screen name="HomeNavigator" component={HomeNavigator} options={{ tabBarLabel: 'Home' }} />
+            <Tab.Screen name="HomeNavigator" component={HomeNavigator} options={{ tabBarLabel: 'Home', tabBarBadge: 3 }} />
         </Tab.Navigator>
     )
 }

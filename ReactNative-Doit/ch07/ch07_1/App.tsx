1c94f9d9234871b43325366fb6e32f22d4c3b8f9
bonjin-app <bonjin.app@gmail.com>
2021-09-29 10:57:34 +0900

    Update App.tsx

--- ReactNative-Doit/ch07/ch07_1/App.tsx
+++ ReactNative-Doit/ch07/ch07_1/App.tsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+import { enableScreens } from 'react-native-screens'
+import { SafeAreaProvider } from 'react-native-safe-area-context'
+
 import MainNavigator from './src/screens/MainNavigator'
+
+enableScreens()
 
 const App = () => {
   return (
-    <MainNavigator />
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <MainNavigator />
+      </NavigationContainer>
+    </SafeAreaProvider>
   )
 }
 

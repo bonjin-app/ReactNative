a484c3848a16bc249a068171b937f0cad07c9a5c
bonjin-app <bonjin.app@gmail.com>
2021-09-29 14:54:08 +0900

    Add NavigationHeader

--- ReactNative-Doit/ch07/ch07_2/src/screens/MainNavigator.tsx
+++ ReactNative-Doit/ch07/ch07_2/src/screens/MainNavigator.tsx
@@ -3,12 +3,17 @@ import Home from './Home'
 import HomeLeft from './HomeLeft'
 import HomeRight from './HomeRight'
 import { createStackNavigator } from '@react-navigation/stack'
+import { Colors } from 'react-native-paper'
 
 const Stack = createStackNavigator()
 
 const MainNavigator = () => {
     return (
-        <Stack.Navigator>
+        <Stack.Navigator
+            screenOptions={{
+                headerShown: false
+            }}
+        >
             <Stack.Screen name="Home" component={Home} />
             <Stack.Screen name="HomeLeft" component={HomeLeft} />
             <Stack.Screen name="HomeRight" component={HomeRight} />

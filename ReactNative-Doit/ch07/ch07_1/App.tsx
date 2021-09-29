b7ef093992bdae5b62762205c5a2ce7e22b1ce65
bonjin-app <bonjin.app@gmail.com>
2021-09-29 11:06:46 +0900

    Update MainNavigation

--- ReactNative-Doit/ch07/ch07_1/App.tsx
+++ ReactNative-Doit/ch07/ch07_1/App.tsx
@@ -1,19 +1,35 @@
-import React from 'react'
-import { NavigationContainer } from '@react-navigation/native'
+import React, { useCallback, useState } from 'react'
+import { NavigationContainer, DarkTheme, DefaultTheme } from '@react-navigation/native'
 import { enableScreens } from 'react-native-screens'
 import { SafeAreaProvider } from 'react-native-safe-area-context'
 
 import MainNavigator from './src/screens/MainNavigator'
+import { AppearanceProvider } from 'react-native-appearance'
+import { ToggleThemeProvider } from './src/contexts'
+import { useColorScheme } from 'react-native'
 
 enableScreens()
 
 const App = () => {
+  const scheme = useColorScheme()
+  const [theme, setTheme] = useState(scheme === 'dark' ? DarkTheme : DefaultTheme)
+
+  const toggleTheme = useCallback(() => {
+    setTheme(({ dark }) => (
+      dark ? DefaultTheme : DarkTheme
+    ))
+  }, [])
+
   return (
-    <SafeAreaProvider>
-      <NavigationContainer>
-        <MainNavigator />
-      </NavigationContainer>
-    </SafeAreaProvider>
+    <AppearanceProvider>
+      <ToggleThemeProvider toggleTheme={toggleTheme}>
+        <SafeAreaProvider>
+          <NavigationContainer theme={theme}>
+            <MainNavigator />
+          </NavigationContainer>
+        </SafeAreaProvider>
+      </ToggleThemeProvider>
+    </AppearanceProvider>
   )
 }
 
--- ReactNative-Doit/ch07/ch07_1/src/screens/MainNavigator.tsx
+++ ReactNative-Doit/ch07/ch07_1/src/screens/MainNavigator.tsx
@@ -1,21 +1,49 @@
+import { useTheme } from '@react-navigation/native'
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Switch, Text, View } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { useToggleTheme } from '../contexts'
 
 const MainNavigator = () => {
+    const theme = useTheme()
+    const toggleTheme = useToggleTheme()
+
     return (
-        <View style={[styles.view]}>
-            <Text>This is top text.</Text>
-            <Text>This is bottm text.</Text>
-        </View>
+        <SafeAreaView style={[styles.view]}>
+            <View style={[styles.view, { backgroundColor: theme.colors.background }]}>
+                <View style={[styles.topBar, { backgroundColor: theme.colors.primary }]}>
+                    <Switch value={theme.dark} onValueChange={toggleTheme} />
+                </View>
+
+                <View style={[styles.view]}>
+                    <Text style={[styles.text, { color: theme.colors.text }]}>This is top text.</Text>
+                    <Text style={[styles.text, { color: theme.colors.text }]}>This is bottom text.</Text>
+                </View>
+            </View>
+
+
+        </SafeAreaView>
     )
 }
 
 export default MainNavigator
 
 const styles = StyleSheet.create({
+    flex: {
+        flex: 1
+    },
     view: {
         flex: 1,
         alignItems: 'center',
         justifyContent: 'space-between',
+    },
+    topBar: {
+        width: '100%',
+        flexDirection: 'row',
+        padding: 5,
+        justifyContent: 'flex-end'
+    },
+    text: {
+        fontSize: 20
     }
 })

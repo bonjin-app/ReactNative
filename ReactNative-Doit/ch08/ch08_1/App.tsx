c4552ddc881c051992e871eaf5deb71cd7fdd395
bonjin-app <bonjin.app@gmail.com>
2021-10-01 16:16:26 +0900

    Update App.tsx

--- ReactNative-Doit/ch08/ch08_1/App.tsx
+++ ReactNative-Doit/ch08/ch08_1/App.tsx
@@ -1,11 +1,42 @@
-import React from 'react'
+import React, { useCallback, useState } from 'react'
+import { NavigationContainer, DarkTheme, DefaultTheme } from '@react-navigation/native'
+import { enableScreens } from 'react-native-screens'
+import { SafeAreaProvider } from 'react-native-safe-area-context'
 import { Provider as ReduxProvider } from 'react-redux'
+
+import MainNavigator from './src/screens/MainNavigator'
+import { AppearanceProvider } from 'react-native-appearance'
+import { ToggleThemeProvider } from './src/contexts'
+import { useColorScheme } from 'react-native'
 import { makeStore } from './src/store'
+
+enableScreens()
 
 const store = makeStore()
 
-export default function App() {
+const App = () => {
+  const scheme = useColorScheme()
+  const [theme, setTheme] = useState(scheme === 'dark' ? DarkTheme : DefaultTheme)
+
+  const toggleTheme = useCallback(() => {
+    setTheme(({ dark }) => (
+      dark ? DefaultTheme : DarkTheme
+    ))
+  }, [])
+
   return (
-    <ReduxProvider store={ store}></ReduxProvider>
+    <AppearanceProvider>
+      <ToggleThemeProvider toggleTheme={toggleTheme}>
+        <SafeAreaProvider>
+          <ReduxProvider store={ store}>
+            <NavigationContainer theme={theme}>
+              <MainNavigator />
+            </NavigationContainer>
+          </ReduxProvider>
+        </SafeAreaProvider>
+      </ToggleThemeProvider>
+    </AppearanceProvider>
   )
 }
+
+export default App

3f7412057bc8f702cdddc9ddc74371d556d02393
bonjin-app <bonjin.app@gmail.com>
2021-10-05 14:15:27 +0900

    Update Base Project

---
+++ ReactNative-Doit/ch06/ch06_2/src/screens/MainNavigator.tsx
@@ -0,0 +1,30 @@
+import React, {useState} from 'react'
+import {BottomNavigation} from 'react-native-paper'
+import Arithmetic from './Arithmetic'
+import Carousel from './Carousel'
+import InsideLayout from './InsideLayout'
+import Transform from './Transform'
+
+export default function MainNavigator() {
+  const [index, setIndex] = useState<number>(0)
+  const [routes] = useState([
+    {key: 'transform', title: 'Transform', icon: 'arrow-collapse-right'},
+    {key: 'layout', title: 'InsideLayout', icon: 'animation-play-outline'},
+    {key: 'arithmetic', title: 'Arithmetic', icon: 'format-annotaion-plus'},
+    {key: 'carousel', title: 'Carousel', icon: 'border-vertical'},
+  ])
+  const renderScene = BottomNavigation.SceneMap({
+    transform: Transform,
+    layout: InsideLayout,
+    arithmetic: Arithmetic,
+    carousel: Carousel
+  })
+
+  return (
+    <BottomNavigation
+      navigationState={{index, routes}}
+      onIndexChange={setIndex}
+      renderScene={renderScene}
+    />
+  )
+}

56e686895ac26fcfe0aae4e2f367ae08000ab83c
bonjin-app <bonjin.app@gmail.com>
2021-10-06 16:04:52 +0900

    Update DragAvatar

--- ReactNative-Doit/ch06/ch06_4/src/screens/DragAvatar.tsx
+++ ReactNative-Doit/ch06/ch06_4/src/screens/DragAvatar.tsx
@@ -1,10 +1,11 @@
 import { FC, useMemo } from "react";
 import { Animated, GestureResponderEvent, PanResponderGestureState, Platform } from "react-native";
 import { useScrollEnabled } from "../contexts";
-import { usePanResponder, useStyle, useTransformStyle } from "../hooks";
+import { useAnimatedValueXY, useMonitorAnimatedValueXY, usePanResponder, useStyle, useTransformStyle } from "../hooks";
 import * as D from '../data'
-import { View } from "../theme/paper";
+import { Text, View } from "../theme/paper";
 import { Avatar } from "../components";
+import { styles } from "./Person.style";
 
 type Event = GestureResponderEvent
 type State = PanResponderGestureState
@@ -18,10 +19,12 @@ export type DragAvatarProps = {
 
 const DragAvatar: FC<DragAvatarProps> = ({ size, backgroundColor }) => {
     const [scrollEnabled, setScrollEnabled] = useScrollEnabled()
+    const animValueXY = useAnimatedValueXY()
+    const reaclAnimValueXY = useMonitorAnimatedValueXY(animValueXY)
 
     const transformStyle = useTransformStyle({
-        translateX: 0,
-        translateY: 0
+        translateX: animValueXY.x,
+        translateY: animValueXY.y
     })
 
     const style = useStyle({
@@ -34,10 +37,25 @@ const DragAvatar: FC<DragAvatarProps> = ({ size, backgroundColor }) => {
     })
 
     const avatarUrl = useMemo(() => D.randomAvatarUrl(), [])
-    const panResponder = usePanResponder({})
+    const panResponder = usePanResponder({
+        onPanResponderGrant() {
+            ios && setScrollEnabled(false)
+        },
+        onPanResponderMove(e: Event, s: State) {
+            const { dx, dy } = s
+            animValueXY.setValue({x: dx, y: dy})
+        },
+        onPanResponderRelease() {
+            animValueXY.extractOffset()
+            ios && setScrollEnabled(true)
+        }
+    })
 
     return (
         <View>
+            <Text style={[styles.text]}>
+                {JSON.stringify(reaclAnimValueXY, null, 2)}
+            </Text>
             <Animated.View style={[style, transformStyle]} {...panResponder.panHandlers}>
                 <Avatar uri={avatarUrl} size={ 60}/>
             </Animated.View>

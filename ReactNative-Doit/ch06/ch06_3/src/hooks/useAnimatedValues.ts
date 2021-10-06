1d837eb3b905a5fbbc8cf12d191bedc1606471d9
bonjin-app <bonjin.app@gmail.com>
2021-10-06 13:51:29 +0900

    Add useAnimatedValues

---
+++ ReactNative-Doit/ch06/ch06_3/src/hooks/useAnimatedValues.ts
@@ -0,0 +1,10 @@
+import { Animated } from 'react-native';
+import { useMemo } from "react"
+
+const makeArray = (length: number) => new Array(length).fill(null)
+
+export const useAnimatedValues = (length: number, initialValue: number = 0) => {
+    return useMemo(() => (
+        makeArray(length).map((notUsed) => new Animated.Value(initialValue))
+    ), [])
+}

27ac0821769edf081aa2613f2af0b5e81fab9b54
bonjin-app <bonjin.app@gmail.com>
2021-12-02 10:34:19 +0900

    Add Greeting Component

--- ReactNativeTechnology/LearnReactNative/App.js
+++ ReactNativeTechnology/LearnReactNative/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import {SafeAreaView, View, Text} from 'react-native';
+import Greeting from './components/Greeting';
 
 const App = () => {
   return (
     <SafeAreaView>
-      <View>
-        <Text>Hello React!</Text>
-      </View>
+      <Greeting />
     </SafeAreaView>
   );
 };
---
+++ ReactNativeTechnology/LearnReactNative/components/Greeting.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+const Greeting = () => {
+  return (
+    <View>
+      <Text>안녕하세요 함수 컴포넌트!</Text>
+    </View>
+  );
+};
+
+export default Greeting;
+
+const styles = StyleSheet.create({});

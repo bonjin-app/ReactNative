8ec2233fee88a75b65c806139862669cd82cb2b7
bonjin-app <bonjin.app@gmail.com>
2021-12-02 10:54:37 +0900

    주석

--- ReactNativeTechnology/LearnReactNative/App.js
+++ ReactNativeTechnology/LearnReactNative/App.js
@@ -3,9 +3,13 @@ import {SafeAreaView, View, Text} from 'react-native';
 import Greeting from './components/Greeting';
 
 const App = () => {
+  const name = 'JSX';
   return (
     <SafeAreaView>
-      <Greeting />
+      {/* 주석 */}
+      <Greeting
+        name={name} // 주석
+      />
     </SafeAreaView>
   );
 };

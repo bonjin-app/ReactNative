9234673da0ad3d06159f609ece32fbd49df6cb9c
bonjin-app <bonjin.app@gmail.com>
2021-12-07 16:17:33 +0900

    AddTodo Component

--- ReactNativeTechnology/TodoApp/components/AddTodo.js
+++ ReactNativeTechnology/TodoApp/components/AddTodo.js
@@ -1,10 +1,53 @@
-import React from 'react';
-import {StyleSheet, Text, TextInput, View} from 'react-native';
+import React, {useState} from 'react';
+import {
+  Image,
+  Keyboard,
+  Platform,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableNativeFeedback,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 
 const AddTodo = () => {
+  const [text, setText] = useState('');
+
+  const onPress = () => {
+    setText('');
+    Keyboard.dismiss();
+  };
+
+  const button = (
+    <View style={styles.buttonStyle}>
+      <Image source={require('../assets/icons/add_white/add_white.png')} />
+    </View>
+  );
   return (
     <View style={styles.block}>
-      <TextInput placeholder="할일을 입력하세요." style={styles.input} />
+      <TextInput
+        value={text}
+        onChangeText={setText}
+        placeholder="할일을 입력하세요."
+        style={styles.input}
+        onSubmitEditing={onPress}
+        returnKeyType="done"
+      />
+      {Platform.select({
+        ios: (
+          <TouchableOpacity onPress={onPress} activeOpacity={0.5}>
+            {button}
+          </TouchableOpacity>
+        ),
+        android: (
+          <View style={styles.circleWrapper}>
+            <TouchableNativeFeedback onPress={onPress}>
+              {button}
+            </TouchableNativeFeedback>
+          </View>
+        ),
+      })}
     </View>
   );
 };
@@ -13,15 +56,30 @@ export default AddTodo;
 
 const styles = StyleSheet.create({
   block: {
+    backgroundColor: 'white',
     height: 64,
     paddingHorizontal: 16,
     borderColor: '#bdbdbd',
     borderTopWidth: 1,
     borderBottomWidth: 1,
     justifyContent: 'center',
+    flexDirection: 'row',
   },
   input: {
+    flex: 1,
     fontSize: 16,
     paddingVertical: 8,
+  },
+  buttonStyle: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: 48,
+    height: 48,
+    backgroundColor: '#26a69A',
+    borderRadius: 24,
+  },
+  circleWrapper: {
+    overflow: 'hidden',
+    borderRadius: 24,
   },
 });

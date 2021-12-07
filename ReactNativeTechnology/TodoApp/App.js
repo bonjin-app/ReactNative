bcad54c6e4be05e440e19f8e194a2d67098ea0e1
bonjin-app <bonjin.app@gmail.com>
2021-12-07 11:11:09 +0900

    Add Assets

--- ReactNativeTechnology/TodoApp/App.js
+++ ReactNativeTechnology/TodoApp/App.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 import {SafeAreaProvider, SafeAreaView} from 'react-native-safe-area-context';
+import AddTodo from './components/AddTodo';
 import DateHead from './components/DateHead';
+import Empty from './components/Empty';
 
 const App = () => {
   const today = new Date();
 
   return (
     <SafeAreaProvider>
-      <SafeAreaView edges={['bottom']}>
+      <SafeAreaView edges={['bottom']} style={styles.block}>
         <DateHead date={today} />
+        <Empty />
+        <AddTodo />
       </SafeAreaView>
     </SafeAreaProvider>
   );
@@ -17,4 +21,8 @@ const App = () => {
 
 export default App;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  block: {
+    flex: 1,
+  },
+});
---
+++ ReactNativeTechnology/TodoApp/components/AddTodo.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+const AddTodo = () => {
+  return <View style={styles.block}></View>;
+};
+
+export default AddTodo;
+
+const styles = StyleSheet.create({
+  block: {
+    height: 64,
+    backgroundColor: 'red',
+  },
+});

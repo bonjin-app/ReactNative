4d747a79d0e7e1dbbf76b6bfe78602fff0ef1beb
bonjin-app <bonjin.app@gmail.com>
2021-12-07 16:38:22 +0900

    Add TodoList

---
+++ ReactNativeTechnology/TodoApp/components/TodoList.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import {FlatList, StyleSheet, Text, View} from 'react-native';
+import TodoItem from './TodoItem';
+
+const TodoList = ({todos}) => {
+  return (
+    <FlatList
+      style={styles.list}
+      data={todos}
+      ItemSeparatorComponent={() => <View style={styles.separator}></View>}
+      renderItem={({item}) => (
+        <TodoItem id={item.id} text={item.text} done={item.done} />
+      )}
+      keyExtractor={item => item.id.toString()}
+    />
+  );
+};
+
+export default TodoList;
+
+const styles = StyleSheet.create({
+  lsit: {
+    flex: 1,
+  },
+  separator: {
+    backgroundColor: '#e0e0e0',
+    height: 1,
+  },
+});

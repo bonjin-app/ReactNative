ed57e6a45bdc8ace0dee7208a78865f98f25bdfc
bonjin-app <bonjin.app@gmail.com>
2021-12-07 20:55:29 +0900

    useRoute

--- ReactNativeTechnology/LearnReactNavigation/screens/DetailScreen.js
+++ ReactNativeTechnology/LearnReactNavigation/screens/DetailScreen.js
@@ -1,14 +1,36 @@
-import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {useRoute} from '@react-navigation/core';
+import React, {useEffect} from 'react';
+import {Button, StyleSheet, Text, View} from 'react-native';
 
-const DetailScreen = () => {
+function IDText() {
+  const route = useRoute();
+  return <Text style={styles.text}>id: {route.params.id}</Text>;
+}
+
+const DetailScreen = ({route, navigation}) => {
+  useEffect(() => {
+    navigation.setOptions({
+      title: `상세 정보 - ${route.params.id}`,
+    });
+  }, [navigation, route.params.id]);
+
   return (
-    <View>
-      <Text>DetailScreen</Text>
+    <View style={styles.block}>
+      <IDText />
+      <View style={styles.buttons}>
+        <Button
+          title="다음"
+          onPress={() => navigation.push('Detail', {id: route.params.id + 1})}
+        />
+        <Button title="뒤로가기" onPress={() => navigation.pop()} />
+        <Button title="처음으로" onPress={() => navigation.popToTop()} />
+      </View>
     </View>
   );
 };
 
 export default DetailScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  block: {},
+});

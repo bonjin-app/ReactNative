f6728f129b6a19189dade08d7fb0cee8591ec2bd
bonjin-app <bonjin.app@gmail.com>
2021-12-08 15:06:48 +0900

    Render Props

--- ReactNativeTechnology/DayLog/src/App.js
+++ ReactNativeTechnology/DayLog/src/App.js
@@ -2,11 +2,14 @@ import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import RootStack from './screens/RootStack';
+import LogContext from './contexts/LogContext';
 
 const App = () => {
   return (
     <NavigationContainer>
-      <RootStack />
+      <LogContext.Provider value="안녕하세요">
+        <RootStack />
+      </LogContext.Provider>
     </NavigationContainer>
   );
 };
--- ReactNativeTechnology/DayLog/src/screens/FeedsScreen.js
+++ ReactNativeTechnology/DayLog/src/screens/FeedsScreen.js
@@ -1,16 +1,27 @@
 import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
+import LogContext from '../contexts/LogContext';
 
 const FeedsScreen = () => {
   return (
     <View style={styles.block}>
-      <Text>FeedsScreen</Text>
+      <Box>{value => <Text>{value}</Text>}</Box>
     </View>
   );
 };
+
+function Box({children}) {
+  return <View style={styles.box}>{children('Hello World')}</View>;
+}
 
 export default FeedsScreen;
 
 const styles = StyleSheet.create({
   block: {},
+  box: {
+    borderWidth: 2,
+    padding: 16,
+    borderBottomColor: 'black',
+    marginBottom: 16,
+  },
 });

27df762f3a07533e147eb47e01487dfa13b5f8d3
bonjin-app <bonjin.app@gmail.com>
2021-12-08 15:01:01 +0900

    Tabs Icons

--- ReactNativeTechnology/DayLog/src/screens/MainTab.js
+++ ReactNativeTechnology/DayLog/src/screens/MainTab.js
@@ -4,15 +4,44 @@ import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import FeedsScreen from './FeedsScreen';
 import CalendarScreen from './CalendarScreen';
 import SearchScreen from './SearchScreen';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 const Tab = createBottomTabNavigator();
 
 const MainTab = () => {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Feeds" component={FeedsScreen} />
-      <Tab.Screen name="Calendar" component={CalendarScreen} />
-      <Tab.Screen name="Search" component={SearchScreen} />
+    <Tab.Navigator
+      screenOptions={{
+        tabBarShowLabel: false,
+        tabBarActiveTintColor: '#009688',
+      }}>
+      <Tab.Screen
+        name="Feeds"
+        component={FeedsScreen}
+        options={{
+          tabBarIcon: ({color, size}) => (
+            <Icon name="view-stream" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Calendar"
+        component={CalendarScreen}
+        options={{
+          tabBarIcon: ({color, size}) => (
+            <Icon name="event" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Search"
+        component={SearchScreen}
+        options={{
+          tabBarIcon: ({color, size}) => (
+            <Icon name="search" size={size} color={color} />
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
 };

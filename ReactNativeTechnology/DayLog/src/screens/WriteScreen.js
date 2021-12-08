47286021f919803511b4080ee16152cebb57e9a8
bonjin-app <bonjin.app@gmail.com>
2021-12-08 17:45:53 +0900

    add WriteEditor

--- ReactNativeTechnology/DayLog/src/screens/WriteScreen.js
+++ ReactNativeTechnology/DayLog/src/screens/WriteScreen.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
+import WriteEditor from '../components/WriteEditor';
 import WriteHeader from '../components/WriteHeader';
 
 const WriteScreen = () => {
   return (
     <SafeAreaView style={styles.block}>
       <WriteHeader />
+      <WriteEditor />
     </SafeAreaView>
   );
 };

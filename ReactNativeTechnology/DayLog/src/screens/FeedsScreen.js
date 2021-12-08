cfe8a2214c01dff86af8823b97788eac9b0885a4
bonjin-app <bonjin.app@gmail.com>
2021-12-08 18:14:29 +0900

    Feeds

--- ReactNativeTechnology/DayLog/src/screens/FeedsScreen.js
+++ ReactNativeTechnology/DayLog/src/screens/FeedsScreen.js
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 import FloatingWriteButton from '../components/FloatingWriteButton';
+import LogContext from '../contexts/LogContext';
 
 const FeedsScreen = () => {
+  const {logs} = useContext(LogContext);
+
   return (
     <View style={styles.block}>
       <FloatingWriteButton />

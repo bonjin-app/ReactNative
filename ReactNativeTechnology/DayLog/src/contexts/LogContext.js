0c324d248a9f6981bb3cfc11c64ce190bfc33eeb
bonjin-app <bonjin.app@gmail.com>
2021-12-09 14:57:16 +0900

    AsyncStorage

--- ReactNativeTechnology/DayLog/src/contexts/LogContext.js
+++ ReactNativeTechnology/DayLog/src/contexts/LogContext.js
@@ -1,20 +1,30 @@
-import React from 'react';
+import React, {useEffect, useRef} from 'react';
 import {createContext, useState} from 'react';
 import {v4 as uuidv4} from 'uuid';
+import logsStorage from '../storages/logsStorage';
 
 const LogContext = createContext(null);
 
 export function LogContextProvider({children}) {
-  const [logs, setLogs] = useState(
-    Array.from({length: 10})
-      .map((_, index) => ({
-        id: uuidv4(),
-        title: `Log ${index}`,
-        body: `Log ${index}`,
-        date: new Date().toISOString(),
-      }))
-      .reverse(),
-  );
+  const initialLogsRef = useRef(null);
+  const [logs, setLogs] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      const savedLogs = await logsStorage.get();
+      if (savedLogs) {
+        initialLogsRef.current = savedLogs;
+        setLogs(savedLogs);
+      }
+    })();
+  }, []);
+
+  useEffect(() => {
+    if (logs === initialLogsRef.current) {
+      return;
+    }
+    logsStorage.set(logs);
+  }, [logs]);
 
   const onModify = modified => {
     const nextLogs = logs.map(log => {

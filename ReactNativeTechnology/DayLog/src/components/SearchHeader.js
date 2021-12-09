5790c55947d1e2fc96e8bc95c2c37f4c524b1a71
bonjin-app <bonjin.app@gmail.com>
2021-12-09 13:14:50 +0900

    SearchHeader

---
+++ ReactNativeTechnology/DayLog/src/components/SearchHeader.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import {
+  Pressable,
+  StyleSheet,
+  Text,
+  TextInput,
+  useWindowDimensions,
+  View,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+const SearchHeader = () => {
+  const {width} = useWindowDimensions();
+  return (
+    <View
+      style={[
+        styles.block,
+        {
+          width: width - 32,
+        },
+      ]}>
+      <TextInput
+        style={styles.input}
+        placeholder="검색어를 입력하세요"
+        autoFocus
+      />
+      <Pressable
+        style={({pressed}) => [styles.button, pressed && {opacity: 0.5}]}>
+        <Icon name="cancel" size={20} color="#9e9e9e" />
+      </Pressable>
+    </View>
+  );
+};
+
+export default SearchHeader;
+
+const styles = StyleSheet.create({
+  block: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  input: {flex: 1},
+  button: {marginLeft: 8},
+});

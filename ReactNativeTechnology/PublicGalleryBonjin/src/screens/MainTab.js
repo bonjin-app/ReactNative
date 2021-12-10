8e55b0ebdc4e7ae64ffbebeee383b4c02127f252
bonjin-app <bonjin.app@gmail.com>
2021-12-10 16:35:08 +0900

    Add CameraButton

--- ReactNativeTechnology/PublicGalleryBonjin/src/screens/MainTab.js
+++ ReactNativeTechnology/PublicGalleryBonjin/src/screens/MainTab.js
@@ -5,34 +5,42 @@ import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import HomeStack from './HomeStack';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import MyProfileStack from './MyProfileStack';
+import CameraButton from '../components/CameraButton';
 
 const Tab = createBottomTabNavigator();
 
 const MainTab = () => {
   return (
-    <Tab.Navigator
-      screenOptions={{
-        headerShown: false,
-        tabBarShowLabel: false,
-        tabBarActiveTintColor: '#6200EE',
-      }}>
-      <Tab.Screen
-        name="HomeStack"
-        component={HomeStack}
-        options={{
-          tabBarIcon: ({color}) => <Icon name="home" size={24} color={color} />,
-        }}
-      />
-      <Tab.Screen
-        name="MayProfileStack"
-        component={MyProfileStack}
-        options={{
-          tabBarIcon: ({color}) => (
-            <Icon name="person" size={24} color={color} />
-          ),
-        }}
-      />
-    </Tab.Navigator>
+    <>
+      <View style={styles.block}>
+        <Tab.Navigator
+          screenOptions={{
+            headerShown: false,
+            tabBarShowLabel: false,
+            tabBarActiveTintColor: '#6200EE',
+          }}>
+          <Tab.Screen
+            name="HomeStack"
+            component={HomeStack}
+            options={{
+              tabBarIcon: ({color}) => (
+                <Icon name="home" size={24} color={color} />
+              ),
+            }}
+          />
+          <Tab.Screen
+            name="MayProfileStack"
+            component={MyProfileStack}
+            options={{
+              tabBarIcon: ({color}) => (
+                <Icon name="person" size={24} color={color} />
+              ),
+            }}
+          />
+        </Tab.Navigator>
+      </View>
+      <CameraButton />
+    </>
   );
 };
 
@@ -41,8 +49,6 @@ export default MainTab;
 const styles = StyleSheet.create({
   block: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
   },
   text: {
     fontSize: 24,

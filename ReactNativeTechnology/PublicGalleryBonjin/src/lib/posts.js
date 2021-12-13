ce5e53204e5dc38edd16f077dab0533f33ecf6c9
bonjin-app <bonjin.app@gmail.com>
2021-12-13 10:23:08 +0900

    Infinity Scroll

--- ReactNativeTechnology/PublicGalleryBonjin/src/lib/posts.js
+++ ReactNativeTechnology/PublicGalleryBonjin/src/lib/posts.js
@@ -2,6 +2,8 @@ import firestore from '@react-native-firebase/firestore';
 
 const postsCollection = firestore().collection('posts');
 
+export const PAGE_SIZE = 3;
+
 export function createPost({user, photoURL, description}) {
   return postsCollection.add({
     user,
@@ -12,10 +14,30 @@ export function createPost({user, photoURL, description}) {
 }
 
 export async function getPosts() {
-  const snapshot = await postsCollection.get();
+  const snapshot = await postsCollection
+    .orderBy('createdAt', 'desc')
+    .limit(PAGE_SIZE)
+    .get();
+
   const posts = snapshot.docs.map(doc => ({
     id: doc.id,
     ...doc.data(),
   }));
+  return posts;
+}
+
+export async function getOlderPosts(id) {
+  const cursorDoc = await postsCollection.doc(id).get();
+  const snapshot = await postsCollection
+    .orderBy('createdAt', 'desc')
+    .startAfter(cursorDoc)
+    .limit(PAGE_SIZE)
+    .get();
+
+  const posts = snapshot.docs.map(doc => ({
+    id: doc.id,
+    ...doc.data(),
+  }));
+
   return posts;
 }
--- ReactNativeTechnology/PublicGalleryBonjin/src/screens/FeedScreen.js
+++ ReactNativeTechnology/PublicGalleryBonjin/src/screens/FeedScreen.js
@@ -1,29 +1,62 @@
 import React, {useEffect, useState} from 'react';
-import {FlatList, StyleSheet, Text, View} from 'react-native';
+import {
+  ActivityIndicator,
+  FlatList,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
 import PostCard from '../components/PostCard';
-import {getPosts} from '../lib/posts';
+import {getOlderPosts, getPosts, PAGE_SIZE} from '../lib/posts';
 
 const FeedScreen = () => {
   const [posts, setPosts] = useState(null);
-
-  console.log('posts', posts);
+  const [noMorePost, setNoMorePost] = useState(false);
 
   useEffect(() => {
     getPosts().then(setPosts);
   }, []);
+
+  const onLoadMore = async () => {
+    if (noMorePost || !posts || posts.length < PAGE_SIZE) {
+      return;
+    }
+
+    const lastPost = posts[posts.length - 1];
+    const olderPosts = await getOlderPosts(lastPost.id);
+    if (olderPosts.length < PAGE_SIZE) {
+      setNoMorePost(true);
+    }
+    setPosts(posts.concat(olderPosts));
+  };
 
   return (
     <FlatList
       data={posts}
       renderItem={renderItem}
       keyExtractor={item => item.id}
+      contentContainerStyle={styles.container}
+      onEndReached={onLoadMore}
+      onEndReachedThreshold={0.75}
+      ListFooterComponent={
+        !noMorePost && (
+          <ActivityIndicator style={styles.spinner} size={32} color="#6200EE" />
+        )
+      }
     />
   );
 };
 
 export default FeedScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    paddingBottom: 48,
+  },
+  spinner: {
+    height: 64,
+  },
+});
 
 const renderItem = ({item}) => (
   <PostCard

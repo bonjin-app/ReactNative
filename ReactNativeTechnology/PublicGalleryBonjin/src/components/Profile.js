621f710f0c81a0f41b1a479e6ef89f04d8f45cf8
bonjin-app <bonjin.app@gmail.com>
2021-12-13 14:06:57 +0900

    usePosts hook

--- ReactNativeTechnology/PublicGalleryBonjin/src/components/Profile.js
+++ ReactNativeTechnology/PublicGalleryBonjin/src/components/Profile.js
@@ -2,13 +2,12 @@ import React, {useEffect, useState} from 'react';
 import {
   ActivityIndicator,
   FlatList,
-  Image,
   RefreshControl,
   StyleSheet,
   Text,
   View,
 } from 'react-native';
-import {getNewerPosts, getOlderPosts, getPosts, PAGE_SIZE} from '../lib/posts';
+import useProps from '../hooks/usePosts';
 import {getUser} from '../lib/users';
 import Avatar from './Avatar';
 import PostGridItem from './PostGridItem';
@@ -16,41 +15,11 @@ import PostGridItem from './PostGridItem';
 const renderItem = ({item}) => <PostGridItem post={item} />;
 const Profile = ({userId}) => {
   const [user, setUser] = useState(null);
-  const [posts, setPosts] = useState(null);
-
-  const [noMorePost, setNoMorePost] = useState(false);
-  const [refreshing, setRefreshing] = useState(false);
-
-  const onLoadMore = async () => {
-    if (noMorePost || !posts || posts.length < PAGE_SIZE) {
-      return;
-    }
-    const lastPost = posts[posts.length - 1];
-    const olderPosts = await getOlderPosts(lastPost.id, userId);
-    if (olderPosts.length < PAGE_SIZE) {
-      setNoMorePost(true);
-    }
-    setPosts(posts.concat(olderPosts));
-  };
-
-  const onRefresh = async () => {
-    if (!posts || posts.length === 0 || refreshing) {
-      return;
-    }
-    const firstPost = posts[0];
-    setRefreshing(true);
-    const newerPosts = await getNewerPosts(firstPost.id, userId);
-    setRefreshing(false);
-
-    if (newerPosts.length === 0) {
-      return;
-    }
-    setPosts(newerPosts);
-  };
+  const {posts, noMorePost, refreshing, onLoadMore, onRefresh} =
+    useProps(userId);
 
   useEffect(() => {
     getUser(userId).then(setUser);
-    getPosts({userId}).then(setPosts);
   }, [userId]);
 
   if (!user || !posts) {

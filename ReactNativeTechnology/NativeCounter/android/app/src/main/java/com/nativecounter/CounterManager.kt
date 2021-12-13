8350c8f84558f6fceaac109ef3b5c5da6c8d31ae
bonjin-app <bonjin.app@gmail.com>
2021-12-13 18:34:09 +0900

    Init Project

---
+++ ReactNativeTechnology/NativeCounter/android/app/src/main/java/com/nativecounter/CounterManager.kt
@@ -0,0 +1,18 @@
+package com.nativecounter
+
+import com.facebook.react.uimanager.SimpleViewManager
+import com.facebook.react.uimanager.ThemedReactContext
+
+class CounterManager: SimpleViewManager<CounterView>() {
+    override fun getName(): String {
+        return REACT_CLASS
+    }
+
+    override fun createViewInstance(reactContext: ThemedReactContext): CounterView {
+        return CounterView(reactContext)
+    }
+
+    companion object {
+        const val REACT_CLASS = "Counter"
+    }
+}

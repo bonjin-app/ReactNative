07be497aa729aaa195e1b4149b042cd06cb86ef5
bonjin-app <bonjin.app@gmail.com>
2021-12-15 16:06:16 +0900

    Init Project

---
+++ ReactNativeTechnology/LearnTypeScript/src/MessageForm.tsx
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const MessageForm = () => {
+    const [text, setText] = useState("");
+    return (
+        <View>
+            <Text></Text>
+        </View>
+    );
+};
+
+export default MessageForm;
+
+const styles = StyleSheet.create({});

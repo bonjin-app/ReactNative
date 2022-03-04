68503e2b002174361a02dbe4a62953047413891f
bonjin-app <bonjin.app@gmail.com>
2022-03-04 14:44:12 +0900

    Add Redux

---
+++ ReactNative-Delivery/FoodDeliveryApp/src/AppInner.tsx
@@ -0,0 +1,61 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {useSelector} from 'react-redux';
+import {RootState} from './store/reducer';
+import {NavigationContainer} from '@react-navigation/native';
+import Orders from './pages/Orders';
+import Delivery from './pages/Delivery';
+import Settings from './pages/Settings';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+
+const Tab = createBottomTabNavigator();
+const Stack = createNativeStackNavigator();
+
+const AppInner = () => {
+  const isLoggedIn = useSelector((state: RootState) => !!state.user.email);
+
+  console.log('isLoggedIn', isLoggedIn);
+  return (
+    <NavigationContainer>
+      {isLoggedIn ? (
+        <Tab.Navigator>
+          <Tab.Screen
+            name="Orders"
+            component={Orders}
+            options={{title: '오더 목록'}}
+          />
+          <Tab.Screen
+            name="Delivery"
+            component={Delivery}
+            options={{headerShown: false}}
+          />
+          <Tab.Screen
+            name="Settings"
+            component={Settings}
+            options={{title: '내 정보'}}
+          />
+        </Tab.Navigator>
+      ) : (
+        <Stack.Navigator>
+          <Stack.Screen
+            name="SignIn"
+            component={SignIn}
+            options={{title: '로그인'}}
+          />
+          <Stack.Screen
+            name="SignUp"
+            component={SignUp}
+            options={{title: '회원가입'}}
+          />
+        </Stack.Navigator>
+      )}
+    </NavigationContainer>
+  );
+};
+
+export default AppInner;
+
+const styles = StyleSheet.create({});

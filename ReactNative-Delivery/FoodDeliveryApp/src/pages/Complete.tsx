9db821537e1bf330ca7e891a5138486c21415da5
bonjin-app <bonjin.app@gmail.com>
2022-03-04 10:17:43 +0900

    Add Pages

---
+++ ReactNative-Delivery/FoodDeliveryApp/src/pages/Complete.tsx
@@ -0,0 +1,14 @@
+import {Pressable, StyleSheet, Text, View} from 'react-native';
+import React, {useState} from 'react';
+
+const Complete = () => {
+  return (
+    <View>
+      <Text>Complete</Text>
+    </View>
+  );
+};
+
+export default Complete;
+
+const styles = StyleSheet.create({});
---
+++ ReactNative-Delivery/FoodDeliveryApp/src/pages/Settings.tsx
@@ -0,0 +1,18 @@
+import {Pressable, StyleSheet, Text, View} from 'react-native';
+import React, {useState} from 'react';
+
+const Settings = () => {
+  const [count, setCount] = useState(1);
+
+  return (
+    <View>
+      <Pressable onPress={() => setCount(p => p + 1)}>
+        <Text>{count}</Text>
+      </Pressable>
+    </View>
+  );
+};
+
+export default Settings;
+
+const styles = StyleSheet.create({});

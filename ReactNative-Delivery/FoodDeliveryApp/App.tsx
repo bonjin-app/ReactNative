e4f1e328221fefcef7558694f9b892efdb91a673
bonjin-app <bonjin.app@gmail.com>
2022-03-10 14:54:24 +0900

    Add Sentry

--- ReactNative-Delivery/FoodDeliveryApp/App.tsx
+++ ReactNative-Delivery/FoodDeliveryApp/App.tsx
@@ -3,6 +3,15 @@ import {Provider} from 'react-redux';
 import store from './src/store';
 import AppInner from './src/AppInner';
 
+import * as Sentry from '@sentry/react-native';
+
+Sentry.init({
+  dsn: 'https://f8a90f20695f4e0395aff6fafa65a34a@o1163648.ingest.sentry.io/6251967',
+  // Set tracesSampleRate to 1.0 to capture 100% of transactions for performance monitoring.
+  // We recommend adjusting this value in production.
+  tracesSampleRate: 1.0,
+});
+
 function App() {
   return (
     <Provider store={store}>
@@ -11,4 +20,4 @@ function App() {
   );
 }
 
-export default App;
+export default Sentry.wrap(App);
